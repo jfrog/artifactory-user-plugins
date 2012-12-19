48f273bbaeaa7b721847a34ef5412ae5c6bd0749
JBaruch <jbaruch@sadogursky.com>
2012-12-19 13:39:30 +0200

    size added, comments

--- checkums/checksums.groovy
+++ checkums/checksums.groovy
@@ -28,19 +28,24 @@ download {
     afterDownloadError { Request request ->
         def requestedPath = request.repoPath.path
         if (isExtension(requestedPath, knownAlogrithms)) {
+            //we only support what DigestUtils supports, look for sha*Hex() methods in http://commons.apache.org/codec/apidocs/org/apache/commons/codec/digest/DigestUtils.html
             RepoPath targetRepoPath = create(request.repoPath.repoKey, removeExtension(requestedPath))
             String extension = getExtension(requestedPath)
             def checksumPropertyName = "checksum.$extension"
             String savedChecksum = repositories.getProperty(targetRepoPath, checksumPropertyName)
             if (savedChecksum) {
+                //if the checksum was already calculated and stored in properties, return it instead of the original 404
                 status = 200
+                size = savedChecksum.size()
                 inputStream = new ByteArrayInputStream(savedChecksum.bytes)
             } else {
+                //if property was not found, calculate new checksum, save as property, return it instead of 404
                 def content = repositories.getContent(targetRepoPath)
                 if (content.size >= 0) {
                     String checksum = content.inputStream.withStream { DigestUtils."${extension}Hex"(it) }
                     repositories.setProperty(targetRepoPath, checksumPropertyName, checksum)
                     status = 200
+                    size = checksum.size()
                     inputStream = new ByteArrayInputStream(checksum.bytes)
                 }
             }
