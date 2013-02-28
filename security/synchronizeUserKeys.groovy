a58571523ed5a6757478f4d65af145f29d8897c9
Fred Simon <freds@jfrog.com>
2013-02-28 16:33:10 -0700

    added a way to synchronize private public keys on settings.xml download

---
+++ security/synchronizeUserKeys.groovy
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2011 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@Grapes([
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.6', root = 'http://repo.jfrog.org/artifactory/')
+])
+import groovyx.net.http.HTTPBuilder
+import org.apache.http.HttpRequestInterceptor
+import org.artifactory.api.security.UserGroupService
+import org.artifactory.factory.InfoFactoryHolder
+import org.artifactory.repo.RepoPath
+
+
+/**
+ * 
+ * Date: 2/28/13
+ * Time: 1:29 PM
+ * @author freds
+ */
+
+String syncAdminUser = 'admin'
+String syncAdminPassword = 'password'
+String otherHostRootURL = 'http://localhost:8080/artifactory'
+
+executions {
+    setUserKeys() { params ->
+        log.info "Receiving setUSerKeys with ${params}"
+
+        String username = params?.get('username')?.get(0) as String
+        String email = params?.get('email')?.get(0) as String
+        String privateKey = params?.get('private')?.get(0) as String
+        String publicKey = params?.get('public')?.get(0) as String
+
+        if (!params || !username || !privateKey || !publicKey) {
+            def errMsg = "Parameters username, private and public are missing please provide them like ?params=username=joe|private=AAA"
+            log.error(errMsg)
+            status = 400
+            message = errMsg
+            return
+        }
+
+        // USe the find or create that is used for external LDAP users
+        def userService = ctx.beanForType(UserGroupService.class)
+        def foundUser = userService.findOrCreateExternalAuthUser(username, false)
+        def newUser = InfoFactoryHolder.get().copyUser(foundUser);
+        newUser.setEmail(email)
+        newUser.setPrivateKey(privateKey)
+        newUser.setPublicKey(publicKey)
+        userService.updateUser(newUser)
+
+        status = 200
+        message = "User $username synchronized sucessfully"
+    }
+}
+
+download {
+    altResponse { request, RepoPath responseRepoPath ->
+        if (responseRepoPath.name == 'settings.xml') {
+            // Retrieve current user info object
+            def userService = ctx.beanForType(UserGroupService.class)
+            def currentUser = userService.findUser(security.currentUsername)
+            log.info "Downloading a settings.xml => Synchronizing keys for ${currentUser.username}"
+            def remoteUrl = "${otherHostRootURL}/api/plugins/execute/setUserKeys"
+            // Synchronize the user keys
+            def http = new HTTPBuilder(remoteUrl)
+            http.client.addRequestInterceptor({def httpRequest, def httpContext ->
+                httpRequest.addHeader('Authorization', "Basic ${"${syncAdminUser}:${syncAdminPassword}".getBytes().encodeBase64()}")
+            } as HttpRequestInterceptor)
+            http.post( query : [params : "username=${currentUser.username}|email=${currentUser.email}|private=${currentUser.privateKey}|public=${currentUser.publicKey}"]) {
+                success = { resp ->
+                    log.debug "User ${currentUser.username} was successfully synchronize"
+                }
+                failure = { resp ->
+                    log.error "Unexpected error while synchronizing ${current.username} ${resp}"
+                    status = resp.status
+                }
+            }
+        }
+    }
+}
