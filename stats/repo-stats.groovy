08b08018b8db413a111206814c9a612fd7c47442
Shay Yaakov <shayy@jfrog.com>
2013-08-01 22:44:09 +0300

    Fixing example typo, should be repos not repo

--- stats/repo-stats.groovy
+++ stats/repo-stats.groovy
@@ -29,7 +29,7 @@
 /** 
  * This execution is named 'repoStats' and it will be called by REST by this name
  * The expected (and mandatory) parameter is comma separated list of repos for which the stats will be be queried
- * curl -X POST -uadmin:password "http://localhost:8081/artifactory/api/plugins/execute/repoStats?params=repo=repo,otherRepo"
+ * curl -X POST -uadmin:password "http://localhost:8081/artifactory/api/plugins/execute/repoStats?params=repos=repo,otherRepo"
  */
 
 executions {
