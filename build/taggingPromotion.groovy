c8c70cd3b4e2af1ca1ed110eb74073d21473ec2b
Eli Givoni <elig@jfrog.com>
2013-10-12 00:04:51 +0300

    addidng new plugin

---
+++ build/taggingPromotion.groovy
@@ -0,0 +1,99 @@
+/*
+* This plugin show tagging and promoting war files which can later be resolved by one generic url that filter resolution with matrix params
+*/
+
+import groovy.transform.Field
+import groovy.xml.StreamingMarkupBuilder
+import org.artifactory.build.Artifact
+import org.artifactory.build.BuildRun
+import org.artifactory.build.Dependency
+import org.artifactory.build.DetailedBuildRun
+import org.artifactory.build.Module
+import org.artifactory.build.ReleaseStatus
+import org.artifactory.common.StatusHolder
+import org.artifactory.exception.CancelException
+import org.artifactory.fs.FileInfo
+import org.artifactory.repo.RepoPath
+import org.artifactory.util.StringInputStream
+import static groovy.xml.XmlUtil.serialize
+import static org.artifactory.repo.RepoPathFactory.create
+
+
+
+promotions {
+
+    cloudPromote(users: "jenkins", params: ['aol.staging': '', 'aol.oss': '', 'aol.prod': '', targetRepository: 'cloud-deploy-local']) { buildName, buildNumber, params ->
+        log.info 'Promoting build: ' + buildName + '/' + buildNumber
+
+        def properties = [:]
+        addStagingProperty(params, properties, 'staging')
+        addStagingProperty(params, properties, 'oss')
+        addStagingProperty(params, properties, 'prod')
+        String targetRepo = getStringProperty(params, 'targetRepository', true)
+
+        if (!params || !buildNumber || properties.isEmpty()) {
+            message = 'Please supply build number parameter and at list one aol. parameter .'
+            log.error message
+            status = 400
+            throw new CancelException(message, status)
+        }
+
+        List<BuildRun> buildsRun = builds.getBuilds(buildName, buildNumber, null)
+        if (buildsRun.size() > 1) {
+            cancelPromotion('Found two matching build to promote, please provide build start time', null, 409)
+        }
+
+        def buildRun = buildsRun[0]
+        if (buildRun == null) {
+            cancelPromotion("Build $buildName/$buildNumber was not found, canceling promotion", null, 409)
+        }
+        Set<FileInfo> stageArtifactsList = builds.getArtifactFiles(buildRun)
+        List<RepoPath> targetList = []
+        StatusHolder cstatus
+        stageArtifactsList.each { item ->
+            RepoPath repoPath = item.getRepoPath()
+            if(repoPath.getPath().endsWith('.war')){
+                RepoPath targetRepoPath = create(targetRepo, repoPath.getPath())
+                targetList << targetRepoPath
+                if (!repositories.exists(targetRepoPath)) {
+                    cstatus = repositories.copy(repoPath, targetRepoPath)
+                    if (cstatus.isError()) {
+                        targetList.each { repositories.delete(it) }
+                        message = "Copy of $repoPath failed ${cstatus.getLastError().getMessage()}"
+                        cancelPromotion(message, cstatus.getLastError().getException(), 500)
+                    }
+
+                }
+                properties.each { prop ->
+                    repositories.setProperty(targetRepoPath, prop.key, prop.value)
+                }
+
+            }
+        }
+
+
+    }
+
+}
+
+private addStagingProperty(params, props, String pName) {
+    String aolP = getStringProperty(params, pName, false)
+    if (aolP) {
+        props["aol.$pName".toString()] = aolP
+    }
+}
+
+private String getStringProperty(params, pName, mandatory) {
+    def key = params[pName]
+    def val = key == null ? null : key[0].toString()
+    if (mandatory && val == null) {
+        cancelPromotion("$pName is mandatory paramater", null, 400)
+    }
+    return val
+}
+
+def cancelPromotion(message, Throwable cause, int errorLevel) {
+    log.warn message
+    throw new CancelException(message, cause, errorLevel)
+}
+
