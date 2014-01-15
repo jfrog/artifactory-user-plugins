e127dad304d1c74f6ad7cd5d00ca2b264ffdfdef
freddy33 <freds@jfrog.com>
2014-01-14 17:52:23 -0800

    some fixing on blackduck property flow

--- governance/statusPropertyWorkflow.groovy
+++ governance/statusPropertyWorkflow.groovy
@@ -1,3 +1,4 @@
+import com.google.common.collect.SetMultimap
 import org.artifactory.addon.AddonsManager
 import org.artifactory.addon.blackduck.generic.model.ExternalComponentInfo
 import org.artifactory.addon.blackduck.service.BlackDuckService
@@ -41,7 +42,7 @@ enum GeneralStatuses {
 }
 
 executions {
-    // The BD CC calls the REST API /api/plugins/setStatusProperty?params=id=459201|externalId=:Newtonsoft.Json:5.0.6|status=APPROVED
+    // The BD CC calls the REST API /api/plugins/execute/setStatusProperty?params=id=459201|externalId=:Newtonsoft.Json:5.0.6|status=APPROVED
     setStatusProperty(version: '1.0',
             description:'set a new status value on all files with cc.id or cc.externalid provided',
             httpMethod: 'POST', users: ['blackduck'].toSet(),
@@ -60,64 +61,83 @@ executions {
             message = "No value for the new status was provided!"
             return
         }
-        if (!GeneralStatuses.values().any {it.name() == ccStatus} ) {
+        if (GeneralStatuses.values().any {it.name() == ccStatus} ) {
+            log.debug "Found valid status $ccStatus"
+        } else {
             status = 400
             message = "Status value $ccStatus does not exists in ${GeneralStatuses.values()}!"
             return
         }
 
-        List<RepoPath> found
+        def filter = [:]
         if (id) {
-            found = searches.itemsByProperties(forMap([ID_PROP_NAME: id]))
+            filter.put(ID_PROP_NAME, id)
         } else {
-            found = searches.itemsByProperties(forMap([EXTERNALID_PROP_NAME: externalid]))
+            filter.put(EXTERNALID_PROP_NAME, externalid)
         }
+        List<RepoPath> found = searches.itemsByProperties(forMap(filter))
         if (!found) {
             status = 404
             message = "No artifacts found with id=$id or externalid=$externalid"
             return
         }
+        List<String> results = ['Converted BEGIN']
         found.each {
             log.debug "Setting ${STATUS_PROP_NAME}=${ccStatus} on ${it.getId()}"
             repositories.setProperty(it, STATUS_PROP_NAME, ccStatus)
             // TODO: Activate some other workflow like copy or move or sync
+            results << "${it.getId()}:$ccStatus"
         }
+        results << 'Converted END\n'
+        status = 200
+        message = results.join("\n")
     }
 }
 
 storage {
     afterCreate { ItemInfo item ->
-        RepoPath repoPath = item.repoPath
-        if (isRemote(repoPath.repoKey) && !MavenNaming.isMavenMetadata(repoPath.path)) {
-            repositories.setProperty(repoPath, STATUS_PROP_NAME, GeneralStatuses.NEW.name())
+        try {
+            RepoPath repoPath = item.repoPath
+            if (!item.folder && isRemote(repoPath.repoKey) &&
+                    !MavenNaming.isMavenMetadata(repoPath.path)) {
+                log.debug "Setting status=NEW on ${repoPath.getId()}"
+                repositories.setProperty(repoPath, STATUS_PROP_NAME, GeneralStatuses.NEW.name())
+            }
+        } catch (Exception e) {
+            log.error("Could not set property on $item", e)
         }
     }
 }
 
 jobs {
     // Create the pending CC requests for all NEW artifacts
-    createCodeCenterRequests(interval: 30000, delay: 300000) {
+    createCodeCenterRequests(interval: 30000, delay: 30000) {
         AddonsManager addonsManager = ctx.getBean(AddonsManager.class)
         BlackDuckWebAddon blackDuckWebAddon = addonsManager.addonByType(BlackDuckWebAddon.class)
         if (!blackDuckWebAddon.isEnableIntegration()) {
             log.error "Blackduck integration configuration not done!"
             return
         }
-        log.info "Finding NEW artifacts that needs BlackDuck approval"
+        log.debug "Finding NEW artifacts that needs BlackDuck approval"
         // Here in how to get the CC connection API object
         BlackDuckService bdService = ctx.getBean(BlackDuckService.class)
         CodeCenterServerProxyV6_4_0_Integration ccConn = bdService.blackDuckWSProvider.blackDuckConnectionProvider.getCodeCenterConnection()
-        List<RepoPath> paths = searches.itemsByProperties(forMap([STATUS_PROP_NAME: GeneralStatuses.NEW.name()]))
+        def filter = [:]
+        filter.put(STATUS_PROP_NAME, GeneralStatuses.NEW.name())
+        List<RepoPath> paths = searches.itemsByProperties(forMap(filter))
         paths.each { RepoPath newArtifact ->
             // Be careful that all POM and JARS for the same GAV are retrieved here
+            log.debug "Found new artifact ${newArtifact.getId()} that needs approval!"
             ExternalComponentInfo eci = bdService.getExternalComponentInfo(newArtifact)
             // TODO: Look at the created blackduck.cc properties and create the general request
             // After creating the request set the status to PENDING
-            repositories.setProperty(newArtifact, STATUS_PROP_NAME, GeneralStatuses.NEW.name())
+            log.debug "Setting status=PENDING on ${newArtifact.getId()}"
+            repositories.setProperty(newArtifact, STATUS_PROP_NAME, GeneralStatuses.PENDING.name())
         }
     }
 }
 
 def isRemote(String repoKey) {
+    if (repoKey.endsWith('-cache')) repoKey = repoKey.substring(0,repoKey.length() - 6)
     return repositories.getRemoteRepositories().contains(repoKey)
 }
