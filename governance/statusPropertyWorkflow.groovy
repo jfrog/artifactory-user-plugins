ba6065c7242cf835e7a53a9033f2b8321deda288
freddy33 <freds@jfrog.com>
2014-02-04 21:10:26 -0800

    made BD CC global workflow works against a given Application

--- governance/statusPropertyWorkflow.groovy
+++ governance/statusPropertyWorkflow.groovy
@@ -1,11 +1,21 @@
 import org.artifactory.addon.AddonsManager
 import org.artifactory.addon.blackduck.generic.model.ExternalComponentInfo
+import org.artifactory.addon.blackduck.service.BlackDuckApplicationService
 import org.artifactory.addon.blackduck.service.BlackDuckService
+import org.artifactory.addon.blackduck.service.impl.BlackDuckRequestInfo
+import org.artifactory.addon.blackduck.service.impl.BlackDuckUpdateResult
 import org.artifactory.addon.blackduck.service.impl.CodeCenterServerProxyV6_4_0_Integration
+import org.artifactory.addon.blackduck.service.impl.Utils
 import org.artifactory.addon.wicket.BlackDuckWebAddon
+import org.artifactory.api.license.LicenseInfo
+import org.artifactory.api.module.ModuleInfo
+import org.artifactory.api.repo.RepositoryService
 import org.artifactory.fs.ItemInfo
 import org.artifactory.mime.MavenNaming
 import org.artifactory.repo.RepoPath
+import org.jfrog.build.api.BlackDuckProperties
+import org.jfrog.build.api.Build
+import org.jfrog.build.api.Governance
 
 import static com.google.common.collect.Multimaps.forMap
 
@@ -31,13 +41,18 @@ import static com.google.common.collect.Multimaps.forMap
  * @since 14/01/14
  */
 
-final PROP_NAME_PREFIX = 'blackduck.cc'
-final ID_PROP_NAME = PROP_NAME_PREFIX + '.id'
-final EXTERNALID_PROP_NAME = PROP_NAME_PREFIX + '.externalid'
-final STATUS_PROP_NAME = PROP_NAME_PREFIX + '.status'
+final CC_PROP_NAME_PREFIX = 'blackduck.cc'
+final GLOBAL_CC_APP_NAME = 'GlobalApp'
+final GLOBAL_CC_APP_VERSION = '1.0'
+final ID_PROP_NAME = CC_PROP_NAME_PREFIX + '.id'
+final EXTERNALID_PROP_NAME = CC_PROP_NAME_PREFIX + '.externalid'
+final STATUS_PROP_NAME = CC_PROP_NAME_PREFIX + '.status'
+final REQUEST_ID_PROP_NAME = CC_PROP_NAME_PREFIX + '.' + GLOBAL_CC_APP_NAME + '.' + GLOBAL_CC_APP_VERSION + '.requestId'
+final ERROR_PROP_NAME = CC_PROP_NAME_PREFIX + '.' + GLOBAL_CC_APP_NAME + '.' + GLOBAL_CC_APP_VERSION + '.error'
+final String[] otherProps = ['riskLevel','vulnerabilities']
 
 enum GeneralStatuses {
-    NEW, PENDING, APPROVED, REJECTED, MANUAL_PENDING
+    NEW, PENDING, APPROVED, REJECTED, MANUAL_PENDING, ERROR
 }
 
 executions {
@@ -81,11 +96,17 @@ executions {
             return
         }
         List<String> results = ['Converted BEGIN']
-        found.each {
-            log.debug "Setting ${STATUS_PROP_NAME}=${ccStatus} on ${it.getId()}"
-            repositories.setProperty(it, STATUS_PROP_NAME, ccStatus)
-            // TODO: Activate some other workflow like copy or move or sync
-            results << "${it.getId()}:$ccStatus"
+        found.each { RepoPath repoPath ->
+            log.debug "Setting ${STATUS_PROP_NAME}=${ccStatus} on ${repoPath.getId()}"
+            // Approval activate some other workflow like copy or move or sync => always last
+            otherProps.each { String propName ->
+                def propVal = params?.get(propName)?.get(0) as String
+                if (propVal) {
+                    repositories.setProperty(repoPath, CC_PROP_NAME_PREFIX + '.' + propName, propVal)
+                }
+            }
+            repositories.setProperty(repoPath, STATUS_PROP_NAME, ccStatus)
+            results << "${repoPath.getId()}:$ccStatus"
         }
         results << 'Converted END\n'
         status = 200
@@ -97,8 +118,11 @@ storage {
     afterCreate { ItemInfo item ->
         try {
             RepoPath repoPath = item.repoPath
+            // Activate Code Center workflow only on actual artifacts not pom
             if (!item.folder && isRemote(repoPath.repoKey) &&
-                    !MavenNaming.isMavenMetadata(repoPath.path)) {
+                    !MavenNaming.isMavenMetadata(repoPath.path) &&
+                    !MavenNaming.isPom(repoPath.path)
+            ) {
                 log.debug "Setting status=NEW on ${repoPath.getId()}"
                 repositories.setProperty(repoPath, STATUS_PROP_NAME, GeneralStatuses.NEW.name())
             }
@@ -110,7 +134,7 @@ storage {
 
 jobs {
     // Create the pending CC requests for all NEW artifacts
-    createCodeCenterRequests(interval: 30000, delay: 30000) {
+    createCodeCenterRequests(interval: 300000, delay: 300000) {
         AddonsManager addonsManager = ctx.getBean(AddonsManager.class)
         BlackDuckWebAddon blackDuckWebAddon = addonsManager.addonByType(BlackDuckWebAddon.class)
         if (!blackDuckWebAddon.isEnableIntegration()) {
@@ -119,19 +143,69 @@ jobs {
         }
         log.debug "Finding NEW artifacts that needs BlackDuck approval"
         // Here in how to get the CC connection API object
+        BlackDuckApplicationService bdAppService = ctx.getBean(BlackDuckApplicationService.class)
         BlackDuckService bdService = ctx.getBean(BlackDuckService.class)
-        CodeCenterServerProxyV6_4_0_Integration ccConn = bdService.blackDuckWSProvider.blackDuckConnectionProvider.getCodeCenterConnection()
+        RepositoryService repoService = ctx.getBean(RepositoryService.class)
+        //CodeCenterServerProxyV6_4_0_Integration ccConn = bdService.blackDuckWSProvider.blackDuckConnectionProvider.getCodeCenterConnection()
         def filter = [:]
         filter.put(STATUS_PROP_NAME, GeneralStatuses.NEW.name())
         List<RepoPath> paths = searches.itemsByProperties(forMap(filter))
+        Map<String,BlackDuckRequestInfo> requestsByGav = [:]
         paths.each { RepoPath newArtifact ->
             // Be careful that all POM and JARS for the same GAV are retrieved here
             log.debug "Found new artifact ${newArtifact.getId()} that needs approval!"
-            ExternalComponentInfo eci = bdService.getExternalComponentInfo(newArtifact)
-            // TODO: Look at the created blackduck.cc properties and create the general request
-            // After creating the request set the status to PENDING
-            log.debug "Setting status=PENDING on ${newArtifact.getId()}"
-            repositories.setProperty(newArtifact, STATUS_PROP_NAME, GeneralStatuses.PENDING.name())
+            ModuleInfo moduleInfo = repoService.getItemModuleInfo(newArtifact);
+            String gav = Utils.getGav(moduleInfo);
+            if (!requestsByGav.containsKey(gav)) {
+                ExternalComponentInfo eci = bdService.getExternalComponentInfo(newArtifact)
+                def req = new BlackDuckRequestInfo();
+                req.published = false
+                req.repoPath = newArtifact
+                req.license = LicenseInfo.UNKNOWN.getName()
+                req.gav = gav
+                req.componentName = moduleInfo.getModule();
+                req.componentVersion = moduleInfo.getBaseRevision();
+                if (eci?.componentId) {
+                    req.componentId = eci.componentId
+                    req.componentName = eci.name
+                    req.componentVersion = eci.version
+                    if (eci.licenses) {
+                        req.license = eci.licenses[0]
+                    }
+                }
+                req.status = BlackDuckRequestInfo.Status.Missing
+                requestsByGav.put(gav, req)
+            }
+        }
+        if (requestsByGav) {
+            def build = new Build()
+            build.name = GLOBAL_CC_APP_NAME
+            build.number = "${System.currentTimeMillis()}"
+            build.setGovernance(new Governance())
+            def blackDuckProperties = new BlackDuckProperties()
+            blackDuckProperties.appName = GLOBAL_CC_APP_NAME
+            blackDuckProperties.appVersion = GLOBAL_CC_APP_VERSION
+            blackDuckProperties.includePublishedArtifacts = false
+            blackDuckProperties.autoCreateMissingComponentRequests = true
+            blackDuckProperties.autoDiscardStaleComponentRequests = false
+            build.governance.blackDuckProperties = blackDuckProperties
+
+            log.debug "Updating ${requestsByGav.size()} requests of $GLOBAL_CC_APP_NAME:$GLOBAL_CC_APP_VERSION"
+            def res = bdAppService.updateRequests(build, requestsByGav.values() as List)
+
+            // After creating the requests set the status to PENDING or ERROR
+            res.each { BlackDuckUpdateResult result ->
+                def repoPath = result.requestInfo.repoPath
+                if (result.updateStatus == BlackDuckUpdateResult.UpdateStatus.create) {
+                    log.debug "Setting status=PENDING requestId=${result.message} on ${repoPath.getId()}"
+                    repositories.setProperty(repoPath, STATUS_PROP_NAME, GeneralStatuses.PENDING.name())
+                    repositories.setProperty(repoPath, REQUEST_ID_PROP_NAME, result.message)
+                } else {
+                    log.error "Setting status=ERROR error=${result.message} on ${repoPath.getId()}"
+                    repositories.setProperty(repoPath, STATUS_PROP_NAME, GeneralStatuses.ERROR.name())
+                    repositories.setProperty(repoPath, ERROR_PROP_NAME, result.message)
+                }
+            }
         }
     }
 }
