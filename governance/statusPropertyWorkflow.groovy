a43ed0c089143068223715966b6409cd1909a42a
liorha <liorha@jfrog.com>
2014-05-08 12:01:31 +0300

    fixing the copyrights to apache

--- governance/statusPropertyWorkflow.groovy
+++ governance/statusPropertyWorkflow.groovy
@@ -19,14 +19,14 @@ import org.jfrog.build.api.Governance
 import static com.google.common.collect.Multimaps.forMap
 
 /*
- * Copyright 2013 JFrog Ltd.
- * 
+ * Copyright (C) 2014 JFrog Ltd.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
