ffe40f62fd7c5d1bdc1f98b20196e8cbdc9035b7
Michal Reuven <michalr@jfrog.com>
2014-10-22 14:17:13 -0700

    Added DB directory and new groovy file to send notification whenever a config change or acl change was performed.

---
+++ DB/DBSyncNotification.groovy
@@ -0,0 +1,34 @@
+import org.artifactory.addon.AddonsManager
+import org.artifactory.addon.HaAddon
+import org.artifactory.addon.ha.message.HaMessageTopic
+
+
+/**
+ * * Copyright 2014 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This plugin will send a notification on CONFIG_CHANGE_TOPIC and ACL_CHANGE_TOPIC
+
+ * Created by Michal on 10/20/2014.
+ */
+executions {
+    syncNotification(description: 'send notification once the permission target was changed') {
+        AddonsManager addonsManager = ctx.beanForType(AddonsManager.class)
+
+        addonsManager.addonByType(HaAddon.class).notify(HaMessageTopic.CONFIG_CHANGE_TOPIC, null)
+        addonsManager.addonByType(HaAddon.class).notify(HaMessageTopic.ACL_CHANGE_TOPIC, null)
+    }
+
+
+}
