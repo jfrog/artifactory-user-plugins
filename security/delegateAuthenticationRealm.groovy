bc19a4d5e2ba5b4b8e27ac7eeb592d202a07f90d
Aaron Rhodes <aaronr@jfrog.com>
2015-05-20 12:06:13 -0700

    Using manual JSonSlurper and basic HTTP request
    Fix request issue and upgrade to builder 0.7.2

--- security/delegateAuthenticationRealm.groovy
+++ security/delegateAuthenticationRealm.groovy
@@ -15,52 +15,70 @@
  */
 
 @Grapes([
-        @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.6')
+        @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.2')
 ]) @GrabExclude('commons-codec:commons-codec')
 
 import groovyx.net.http.ContentType
+import groovyx.net.http.Method
 import groovyx.net.http.HTTPBuilder
-import org.apache.http.HttpRequestInterceptor
 import org.artifactory.security.User
+import groovy.transform.Field
 
 /**
  *
  * Date: 2/4/15
-  * @author Michal
+ * @author Fred Simon
  */
 
-String centralAuthenticatorArtifactory = 'http://artifactory.inc.jfrog.local:8081/artifactory'
+@Field final String centralAuthenticatorArtifactory = 'http://localhost:8081'
+@Field final String uriPath = '/artifactory/api/plugins/execute/getCurrentUserDetails'
 
 
 realms {
     delegateRealm(autoCreateUsers: true) {
         authenticate { username, credentials ->
             if (username == 'anonymous' || username == 'admin') return
-            log.info "Authenticating '${username}/${credentials}' against $centralAuthenticatorArtifactory ..."
+            log.info "Authenticating '${username}' against ${centralAuthenticatorArtifactory}/${uriPath} ..."
 
-            def http = new HTTPBuilder("${centralAuthenticatorArtifactory}/")
-            http.client.addRequestInterceptor({ def httpRequest, def httpContext ->
-                httpRequest.addHeader('Authorization', "Basic ${"${username}:${credentials}".getBytes().encodeBase64()}")
-            } as HttpRequestInterceptor)
+            def http = new HTTPBuilder(centralAuthenticatorArtifactory)
             http.parser.'application/json' = http.parser.'text/plain'
             http.encoder[ContentType.ANY] = { it }
 
-            http.get(path: 'api/plugins/execute/getCurrentUserDetails' ) {
-                success = { resp, json ->
-                    // Populate user from remote details
-                    User currentUser = getUser()
-                    currentUser.privateKey = json.privateKey
-                    currentUser.publicKey = json.publicKey
-                    currentUser.email = json.email
-                    currentUser.groups = json.groups
-                    log.info "User ${currentUser.username} was successfully synchronize"
+            boolean passed = false
+
+            try {
+                def slurper = new groovy.json.JsonSlurper()
+                User currentUser = getUser()
+                log.debug "Calling get on getCurrentUserDetails for ${currentUser}"
+
+                http.request(Method.GET, ContentType.TEXT) {
+                    uri.path = uriPath
+                    headers.'Authorization' = 'Basic ' + "${username}:${credentials}".getBytes().encodeBase64()
+                    log.debug "Using ${uri} and ${headers}"
+                    response.success = { resp, is ->
+                        log.debug "Received data from remote for ${username} with status ${resp.statusLine}"
+                        // Populate user from remote details
+                        def json = slurper.parse(is)
+                        currentUser.privateKey = json.privateKey
+                        currentUser.publicKey = json.publicKey
+                        currentUser.email = json.email
+                        currentUser.groups = json.groups
+                        log.info "User ${currentUser.username} was successfully synchronize"
+                        passed = true
+                    }
+                    response.failure = { resp ->
+                        if (resp.status == 401) {
+                        	log.info "User ${username} provided wrong credentials got: ${resp.statusLine}"
+                        } else {
+                        	log.error "Unexpected error while synchronizing ${username} ${resp.statusLine}"
+                        }
+                    }
                 }
-                failure = { resp ->
-                    log.error "Unexpected error while synchronizing ${current.username} ${resp}"
-                    status = resp.status
-                }
+            } catch (Throwable th) {
+                log.error(th.getMessage(), th)
             }
-            true
+
+            return passed
         }
 
         userExists { username ->
@@ -69,5 +87,3 @@ realms {
         }
     }
 }
-
-
