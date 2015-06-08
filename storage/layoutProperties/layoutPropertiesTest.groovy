8a41230c011fabe16b4dc4fe8f83f33e00e10bba
travisf <travisf@jfrog.com>
2015-06-08 12:12:04 -0700

    Added working test for layoutProperties plugin

--- storage/layoutProperties/layoutPropertiesTest.groovy
+++ storage/layoutProperties/layoutPropertiesTest.groovy
@@ -2,14 +2,23 @@ import static org.jfrog.artifactory.client.ArtifactoryClient.create
 import spock.lang.Specification
 
 class LayoutPropertiesTest extends Specification {
-    def 'test name'() {
+    def 'maven layout properties plugin test'() {
         setup:
+        def repo = 'libs-release-local'
+        def path = 'org/test/modname/1.0/modname-1.0.txt'
         def artifactory = create("http://localhost:8088/artifactory", "admin", "password")
 
-        //when:
+        when:
+        def stream = new ByteArrayInputStream('test'.getBytes('utf-8'))
+        artifactory.repository(repo).upload(path, stream).doUpload()
+        def props = artifactory.repository(repo).file(path).getProperties('')
 
-        //then:
+        then:
+        props['layout.organization'] == ['org.test']
+        props['layout.module'] == ['modname']
+        props['layout.baseRevision'] == ['1.0']
 
-        //cleanup:
+        cleanup:
+        artifactory.repository(repo).delete('org')
     }
 }
