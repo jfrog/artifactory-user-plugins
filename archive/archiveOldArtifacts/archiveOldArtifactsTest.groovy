631a0da35d22c52047befb43db24b2a5cabdaed0
travisf <travisf@jfrog.com>
2015-06-09 18:11:11 -0700

    Added working test for archiveOldArtifacts plugin

--- archive/archiveOldArtifacts/archiveOldArtifactsTest.groovy
+++ archive/archiveOldArtifacts/archiveOldArtifactsTest.groovy
@@ -2,14 +2,22 @@ import static org.jfrog.artifactory.client.ArtifactoryClient.create
 import spock.lang.Specification
 
 class ArchiveOldArtifactsTest extends Specification {
-    def 'test name'() {
+    def 'archive old artifacts plugin test'() {
         setup:
         def artifactory = create("http://localhost:8088/artifactory", "admin", "password")
+        def stream = new ByteArrayInputStream('test'.getBytes('utf-8'))
+        artifactory.repository('libs-release-local').upload('foo.txt', stream).doUpload()
+        artifactory.repository('libs-release-local').file('foo.txt').properties().addProperty('archive', 'yes').doSet()
 
-        //when:
+        when:
+        def pt1 = "http://localhost:8088/artifactory/api/plugins/execute/archive_old_artifacts"
+        def pt2 = "params=includePropertySet=archive|srcRepo=libs-release-local|archiveRepo=plugins-release-local"
+        "curl -X POST -uadmin:password $pt1?$pt2".execute().waitFor()
 
-        //then:
+        then:
+        artifactory.repository('plugins-release-local').file('foo.txt').getPropertyValues('archived.timestamp')
 
-        //cleanup:
+        cleanup:
+        artifactory.repository('plugins-release-local').delete('foo.txt')
     }
 }
