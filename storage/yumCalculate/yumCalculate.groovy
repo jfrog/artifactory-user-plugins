5b0e6c388285899da7481cdcd918d75fe31c9a1c
freddy33 <freds@jfrog.com>
2015-06-11 18:33:38 -0700

    cleaned and add constraints on input params

--- storage/yumCalculate/yumCalculate.groovy
+++ storage/yumCalculate/yumCalculate.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.artifactory.repo.LocalRepositoryConfiguration
 import org.artifactory.repo.RepoPath
 import org.artifactory.repo.RepoPathFactory
@@ -21,7 +37,19 @@ def calculateYumMetadata(RepoPath repoPath) {
 
 executions {
     yumCalculate() { params ->
-        def err = calculateYumMetadata(RepoPathFactory.create(params.path[0]))
+        String repPath = params?.get('path')?.get(0) as String
+        if (!repPath) {
+            status = 400
+            message = "Need a path parameter to calculate yum metadata"
+            return
+        }
+        def repoPath = RepoPathFactory.create(repPath)
+        if (!repositories.exists(repoPath)) {
+            status = 404
+            message = "Folder $repoPath.id to index YUM does not exists"
+            return
+        }
+        def err = calculateYumMetadata(repoPath)
         if (err) {
             status = 403
             message = err
