69a07792464125ff9bd3536b0cc591e3e77723a9
travisf <travisf@jfrog.com>
2015-06-18 11:06:05 -0700

    Fixed modifyMD5File comments
    Deleted the duplicate license block, and copied the correct documentation
    comment from commit 6fbcde40692e88f5655e5252b22a3367a151201b, where it was
    incorrectly applied to the internalRewriteDownload plugin.

--- download/modifyMD5File/modifyMD5File.groovy
+++ download/modifyMD5File/modifyMD5File.groovy
@@ -17,27 +17,9 @@ import org.artifactory.repo.RepoPath
 import org.artifactory.repo.RepoPathFactory
 import org.artifactory.request.Request
 
-/*
- * Copyright 2014 JFrog Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /**
- * An example on how to use internal rewrite plugin.
- * Here requests to dist-local/latest/XXX will be redirected (without sending a 302 to the client) to
- * dist-local/[folder]/XXX where folder is the value of the 'latest.folderName' property on the root
- * folder dist-local.
+ * Here all files under md5-test-remote that ends with '.md5' will be modified
+ * to '.md5.txt'
  *
  * Date: 3/13/14 3:43 PM
  * @author freds
