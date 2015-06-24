eda910bf93686536973d4a2c7c10906d52f515bb
travisf <travisf@jfrog.com>
2015-06-24 11:47:43 -0700

    Added working test for yumCalculate plugin

--- storage/yumCalculate/yumCalculateTest.groovy
+++ storage/yumCalculate/yumCalculateTest.groovy
@@ -1,15 +1,27 @@
+import groovyx.net.http.HttpResponseException
+import org.jfrog.artifactory.client.model.builder.impl.RepositoryBuildersImpl
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 import spock.lang.Specification
 
 class YumCalculateTest extends Specification {
-    def 'test name'() {
+    def 'simple yum calculate test'() {
         setup:
         def artifactory = create("http://localhost:8088/artifactory", "admin", "password")
+        def builder = RepositoryBuildersImpl.create()
+        def yum = builder.localRepositoryBuilder().key('yum').yumRootDepth(2).calculateYumMetadata(false).build()
+        artifactory.repositories().create(0, yum)
+        artifactory.repository('yum').folder('org/mod1').create()
+        artifactory.repository('yum').folder('org/mod2').create()
 
-        //when:
+        when:
+        artifactory.plugins().execute('yumCalculate').withParameter('path', 'yum/org/mod1').sync()
+        artifactory.repository('yum').folder('org/mod2/repodata').info()
 
-        //then:
+        then:
+        thrown(HttpResponseException)
+        artifactory.repository('yum').folder('org/mod1/repodata').info()
 
-        //cleanup:
+        cleanup:
+        artifactory.repository('yum').delete()
     }
 }
