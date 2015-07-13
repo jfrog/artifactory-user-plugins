0f5e08ce4aa85ab11824fe83bb03e34b36d0a8b4
travisf <travisf@jfrog.com>
2015-07-13 12:15:08 -0700

    Updated setup scripts
    Updated the setup.groovy scripts to use the new 'installLibraries'
    function defined by the devenv.

--- deploy/remoteDownload/setup.groovy
+++ deploy/remoteDownload/setup.groovy
@@ -1,7 +1,4 @@
 // install dependency libraries
-def libdir = new File('./etc/plugins/lib')
-libdir.mkdir()
-def url1 = new URL('https://jcenter.bintray.com/org/codehaus/groovy/modules/http-builder/http-builder/0.7.2/http-builder-0.7.2.jar')
-def url2 = new URL('https://jcenter.bintray.com/net/sf/json-lib/json-lib/2.4/json-lib-2.4-jdk15.jar')
-new File(libdir, 'http-builder-0.7.2.jar').newOutputStream() << url1.openStream()
-new File(libdir, 'json-lib-2.4-jdk15.jar').newOutputStream() << url2.openStream()
+lib1 = 'org/codehaus/groovy/modules/http-builder/http-builder/0.7.2/http-builder-0.7.2.jar'
+lib2 = 'net/sf/json-lib/json-lib/2.4/json-lib-2.4-jdk15.jar'
+installLibraries([lib1, lib2])
