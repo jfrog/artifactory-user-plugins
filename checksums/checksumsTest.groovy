ca84949c6e91476fbc72a094182aab60f712f7cb
travisf <travisf@jfrog.com>
2015-09-14 14:59:09 -0700

    Added test for checksums plugin

--- checksums/checksumsTest.groovy
+++ checksums/checksumsTest.groovy
@@ -1,15 +1,24 @@
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.apache.commons.codec.digest.DigestUtils
 import spock.lang.Specification
 
 class ChecksumsTest extends Specification {
-    def 'test name'() {
+    def 'checksums test'() {
         setup:
+        def message = 'Lorem ipsum dolor sit amet'
+        def checksum = DigestUtils.sha512Hex(message)
         def artifactory = create("http://localhost:8088/artifactory", "admin", "password")
+        def repo = artifactory.repository('libs-release-local')
+        def data = new ByteArrayInputStream(message.bytes)
+        repo.upload('testfile', data).doUpload()
 
-        //when:
+        when:
+        def result = repo.download('testfile.sha512').doDownload().text
 
-        //then:
+        then:
+        checksum == result
 
-        //cleanup:
+        cleanup:
+        repo.delete('testfile')
     }
 }
