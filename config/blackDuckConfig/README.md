6080a4ed2dd882d901276aa3fc9617496ff5b0a3
travisf <travisf@jfrog.com>
2015-09-29 16:51:06 -0700

    Added BlackDuck config plugin
    Added plugin for accessing the BlackDuck configuration via the REST api.

---
+++ config/blackDuckConfig/README.md
@@ -0,0 +1,52 @@
+Artifactory Black Duck Config User Plugin
+=========================================
+
+Allows REST access to the Black Duck configuration. This plugin exposes two
+executions:
+
+getBlackDuck
+------------
+
+`getBlackDuck` returns a JSON representation of the current Black Duck
+configuration. The returned JSON string has the following fields:
+
+- `enableIntegration`: Whether Black Duck is enabled.
+- `serverUri`: The URI of the Black Duck server.
+- `username`: The username with which to log in.
+- `password`: The password with which to log in.
+- `connectionTimeoutMillis`: The network timeout to use.
+- `proxy`: The key of the proxy configuration to use.
+
+For example:
+
+```
+$ curl -u admin:password 'http://localhost:8081/artifactory/api/plugins/execute/getBlackDuck'
+{
+    "enableIntegration": true,
+    "serverUri": "somehost",
+    "username": "admin",
+    "password": "password",
+    "connectionTimeoutMillis": 5000,
+    "proxy": "someproxy"
+}
+```
+
+setBlackDuck
+------------
+
+`setBlackDuck` updates the current Black Duck configuration using the provided
+JSON object. This object may have any combination of the fields listed above,
+and only the provided fields will be updated in the configuration.
+
+For example:
+
+```
+$ curl -u admin:password -X POST -H 'Content-Type: application/json' -d '{
+> "enableIntegration": true,
+> "serverUri": "somehost",
+> "username": "admin",
+> "password": "password",
+> "connectionTimeoutMillis": 20000,
+> "proxy": "someproxy"
+> }' 'http://localhost:8081/artifactory/api/plugins/execute/setBlackDuck'
+```
---
+++ config/blackDuckConfig/blackDuckConfigTest.groovy
@@ -0,0 +1,74 @@
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import spock.lang.Specification
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class BlackDuckConfigTest extends Specification {
+    def 'black duck plugin test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory/api/plugins/execute'
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        def conn = new URL("$baseurl/getBlackDuck").openConnection()
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def backup = conn.inputStream.bytes
+        conn.disconnect()
+
+        when:
+        def json1 = [
+            enableIntegration: true, serverUri: 'somehost',
+            username: 'admin', password: 'password',
+            connectionTimeoutMillis: 20000, proxy: null]
+        conn = new URL("$baseurl/setBlackDuck").openConnection()
+        conn.doOutput = true
+        conn.requestMethod = 'POST'
+        conn.setRequestProperty('Authorization', auth)
+        conn.setRequestProperty('Content-Type', 'application/json')
+        conn.outputStream.write(new JsonBuilder(json1).toString().bytes)
+        assert conn.responseCode == 200
+        conn.disconnect()
+        conn = new URL("$baseurl/getBlackDuck").openConnection()
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def reader1r = new InputStreamReader(conn.inputStream)
+        def json1r = new JsonSlurper().parse(reader1r)
+        conn.disconnect()
+
+        then:
+        json1 == json1r
+
+        when:
+        def json2 = [
+            enableIntegration: false, serverUri: 'someotherhost',
+            username: 'admin', password: 'password',
+            connectionTimeoutMillis: 40000, proxy: null]
+        conn = new URL("$baseurl/setBlackDuck").openConnection()
+        conn.doOutput = true
+        conn.requestMethod = 'POST'
+        conn.setRequestProperty('Authorization', auth)
+        conn.setRequestProperty('Content-Type', 'application/json')
+        conn.outputStream.write(new JsonBuilder(json2).toString().bytes)
+        assert conn.responseCode == 200
+        conn.disconnect()
+        conn = new URL("$baseurl/getBlackDuck").openConnection()
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def reader2r = new InputStreamReader(conn.inputStream)
+        def json2r = new JsonSlurper().parse(reader2r)
+        conn.disconnect()
+
+        then:
+        json2 == json2r
+
+        cleanup:
+        conn = new URL("$baseurl/setBlackDuck").openConnection()
+        conn.doOutput = true
+        conn.requestMethod = 'POST'
+        conn.setRequestProperty('Authorization', auth)
+        conn.setRequestProperty('Content-Type', 'application/json')
+        conn.outputStream.write(backup)
+        assert conn.responseCode == 200
+        conn.disconnect()
+    }
+}
