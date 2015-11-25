f257331d8bd2ea28fcd9d84873f148e51b304b75
travisf <travisf@jfrog.com>
2015-11-24 16:29:15 -0800

    Updated getUsersByGroup
    The getUsersByGroup plugin used to do all of its work by making many
    calls to the REST API. This does not scale well. Now, it uses the
    internal apis to make optimized database accesses, which is much faster.

--- security/getUsersByGroup/getUsersByGroup.groovy
+++ security/getUsersByGroup/getUsersByGroup.groovy
@@ -14,11 +14,8 @@
  * limitations under the License.
  */
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64
+import org.artifactory.api.security.UserGroupService
 import groovy.json.JsonBuilder
-import groovy.json.JsonSlurper
-
-import java.nio.charset.Charset
 
 /**
  * This plugin finds all the users that are included in a specific group
@@ -27,14 +24,6 @@ import java.nio.charset.Charset
  * @since 07/21/15
  */
 
-class IncludedUser {
-    String name
-
-    IncludedUser(String name) {
-        this.name = name
-    }
-}
-
 /**
  * The command to get the info:
  * curl -X GET -u{admin_user}:{password} "http://{ARTIFACTORY_URL}:{PORT}/artifactory/api/plugins/execute/getUsersByGroup?params=group={group_name}"
@@ -42,117 +31,29 @@ class IncludedUser {
  */
 executions {
     getUsersByGroup(httpMethod: 'GET') { params ->
-        // Change the URL to your server url
-        String basicUrlString = "http://localhost:8081/artifactory/api/security/"
-        // constracts the URL to get all the users information
-        String urlString = basicUrlString + "users"
-        // retriving the user that performs the request. Most be an admin user.
-        String user = security.getCurrentUsername()
-        String pass = security.getEncryptedPassword()
-        ArrayList<String> groupNameArrayFromParams = params['group']
-        String groupName = groupNameArrayFromParams.get(0)
-
-        log.info("Getting the required users for the group $groupName")
-        // constracts the URL to get group details
-        String getGroupDetails = basicUrlString + "groups/" + groupName
-        // validation if the group exists
-        boolean passValidation = validation(getGroupDetails, user, pass)
-        // if the group does not exists return 400
-        if (!passValidation) {
+        def userGroupService = ctx.beanForType(UserGroupService.class)
+        def groupName = params['group']?.get(0)
+        log.info("Getting the users of the group '$groupName'")
+        // if a group was not specified, return 400
+        if (groupName == null) {
             status = 400
-            message = "No such Group $groupName"
-            log.error("Group $groupName does not exists")
+            message = "A group must be specified"
+            log.error("A group was not specified")
+            return
         }
-
-        // open the connection to the URL and retrieve the json from the call
-        def allUsersJson = getJson(urlString, user, pass)
-        boolean namesOfGroups
-        ArrayList<String> names = allUsersJson.name
-        ArrayList<String> uri = allUsersJson.uri
-        def json = new JsonBuilder()
-
-        int i = 0
-
-        ArrayList<IncludedUser> list = new ArrayList<>()
-        while (i < uri.size()) {
-            // get if the user is included or not.
-            namesOfGroups = getUsersInfo(uri.get(i), groupName, user, pass)
-            // if included add the required info to the list as new object IncludedUser (in the basic plugin the info is the user name)
-            if (namesOfGroups) {
-                list.add(new IncludedUser(names[i]))
-            }
-            i++
+        // if the group does not exist, return 404
+        if (userGroupService.findGroup(groupName) == null) {
+            status = 404
+            message = "No such group '$groupName'"
+            log.error("Group '$groupName' does not exist")
+            return
         }
-        // creating the json
-        json(groupName, list)
-
-        // printing the message to the requested user.
-        message = json.toPrettyString()
-        log.info("Finished retrieving the required users for the group $groupName")
-    }
-}
-
-/**
- * The method returns true if the user is included in the group.
- * true = the user is included in the group. false = not included.
- * @param uri = the URL to retrieve the user information as json (see in Artifactory Wiki REST API Get User Details
- * @param group = the group name that we are checking if the user is included in.
- * @param user = the user that made the request (this is needed to retrieve the json)
- * @param pass = the user password that made the request (this is needed to retrieve the json)
- */
-private boolean getUsersInfo(String uri, String group, String user, String pass) {
-    // get users, parse the user retrieved information and check if the required group is included in the user info.
-    def users = getJson(uri, user, pass)
-    ArrayList<String> usersGroups = users.groups
-    if (usersGroups != null) {
-        ArrayList<String> groups = usersGroups.value
-
-        for (int i = 0; i < groups.size(); i++) {
-            if (group.equalsIgnoreCase(groups.get(i).toString())) {
-                return true
-            }
-        }
-    }
-    return false
-}
-
-/**
- * This method performs the REST call to the required URL and returns the answer as an JSON object
- * @param urlString - the URL to do the REST call
- * @param user - user for authentication
- * @param pass - password for the authentication
- * @return - return the json object of the result.
- */
-private Object getJson(String urlString, String user, String pass) {
-    URL url = new URL(urlString)
-    URLConnection uc = url.openConnection()
-    String userpass = user + ":" + pass
-    String basicAuth = "Basic " + new String(new Base64().encode(userpass.getBytes()))
-    uc.setRequestProperty("Authorization", basicAuth)
-    InputStream is = uc.getInputStream()
-    def json = new JsonSlurper().parse(new InputStreamReader(is, Charset.forName("UTF-8")))
-    // close the connection since we are done using it.
-    is.close()
-    return json
-}
-
-/**
- * This method performs the REST call to the required URL and returns true if the group exists if not then return false
- * @param urlString - the URL to do the REST call to get group info
- * @param user - user for authentication
- * @param pass - password for the authentication
- * @return - return true if the group exists else return false.
- */
-private boolean validation(String urlString, String user, String pass) {
-    try {
-        URL url = new URL(urlString)
-        URLConnection uc = url.openConnection()
-        String userpass = user + ":" + pass
-        String basicAuth = "Basic " + new String(new Base64().encode(userpass.getBytes()))
-        uc.setRequestProperty("Authorization", basicAuth)
-        InputStream is = uc.getInputStream()
-        return true
-    } catch (FileNotFoundException e) {
-        return false
+        // get the list of users who are in the group
+        def users = []
+        def usersInGroup = userGroupService.findUsersInGroup(groupName)
+        for (def user : usersInGroup) users.add([name: user.username])
+        // print the message to the requesting user
+        message = new JsonBuilder([groupName, users]).toPrettyString()
+        log.info("Finished getting the users of the group '$groupName'")
     }
 }
