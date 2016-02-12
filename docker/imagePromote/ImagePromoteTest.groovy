124cd09fea69f8151952f74bef959d74d923b347
travisf <travisf@jfrog.com>
2016-02-12 12:51:44 -0800

    Fixed some file names to match plugin layout
    Fixed the promoteWithDeps, imagePromote, and requestRouting plugins to
    be compatible with the artifactory-user-plugins layout.

---
+++ docker/imagePromote/ImagePromoteTest.groovy
@@ -0,0 +1,17 @@
+import spock.lang.Specification
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class ImagePromoteTest extends Specification {
+    def 'test name'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+
+        // when:
+
+        // then:
+
+        // cleanup:
+    }
+}
