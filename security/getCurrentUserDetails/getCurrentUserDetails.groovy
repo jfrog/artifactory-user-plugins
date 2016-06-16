e74ad2cb5a177699907ffa03a8f603782cabb423
nyadav08 <nyadav@hotmail.com>
2016-06-16 14:20:17 -0700

    Update getCurrentUserDetails.groovy
    Added user security method

--- security/getCurrentUserDetails/getCurrentUserDetails.groovy
+++ security/getCurrentUserDetails/getCurrentUserDetails.groovy
@@ -1,20 +1,11 @@
 /*
- * Copyright (C) 2014 JFrog Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2016 JFrog Ltd. All rights reserved.
+ * JFROG PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
 import groovy.json.JsonBuilder
+import org.artifactory.security.User
 
 /**
  *
@@ -23,9 +14,11 @@ import groovy.json.JsonBuilder
  */
 
 executions {
-    getCurrentUserDetails(httpMethod: 'GET', groups: ['readers']) {
+    getCurrentUserDetails(httpMethod: 'GET', groups:['readers']) {
         log.info "Requesting user details for ${security.currentUsername()}"
-        JsonBuilder builder = new JsonBuilder(security.currentUser())
+        User user = security.currentUser()
+        security.populateUserProperties(user)
+        JsonBuilder builder = new JsonBuilder(user)
         message = builder.toPrettyString()
         log.debug "Returning User Details: ${message}"
         status = 200
