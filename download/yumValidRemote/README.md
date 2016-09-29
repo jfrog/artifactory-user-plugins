778b98d14cb4df677c85ed15f1834caafcb0f30f
travisf <travisf@jfrog.com>
2016-09-29 16:55:03 -0700

    Updated yumValidRemote
    This plugin is now a little better:
    - Internal caching mechanisms are now used to download files, which
      means that options like remote proxies etc are now used properly
    - Old metadata files are kept around, in case the far end starts having
      issues, Artifactory will still continue to serve correct information
    - Files are downloaded in separate threads, reducing some wait times and
      avoiding yum timeouts

--- download/yumValidRemote/README.md
+++ download/yumValidRemote/README.md
@@ -6,8 +6,9 @@ filenames. This can lead to caching issues, which can cause version and checksum
 mismatch. This plugin attempts to alleviate this issue, by transforming repodata
 files on the fly when requested from a remote repository.
 
-Due to a limitation of the Artifactory User Plugin system, it may take a while
-to initially cache some repodata files. This can cause yum to timeout one or
-more times before successfully downloading these files. This should only be a
-problem when Artifactory needs to recache these files. If yum timing out causes
-grief, change the `timeout` option in the `/etc/yum.conf` file.
+The first time this plugin is used, or after the cache is cleared, metadata
+files in the process of downloading may 404. Once this initial download is
+complete, the correct metadata files should always be available, even when there
+are updates on the remote server. This is because the plugin refuses to index
+new metadata until after all the required metadata files are cached, and it will
+continue to use the old metadata in the meantime.
--- download/yumValidRemote/yumValidRemote.groovy
+++ download/yumValidRemote/yumValidRemote.groovy
@@ -14,53 +14,73 @@
  * limitations under the License.
  */
 
+import org.artifactory.api.common.MoveMultiStatusHolder
 import org.artifactory.repo.RepoPathFactory
+import org.artifactory.repo.service.InternalRepositoryService
+import org.artifactory.repo.service.mover.DefaultRepoPathMover
+import org.artifactory.repo.service.mover.MoverConfigBuilder
+import org.artifactory.request.NullRequestContext
+import org.artifactory.schedule.CachedThreadPoolTaskExecutor
 
-import java.security.MessageDigest
+downloadMutex = new Object()
+downloadQueue = [:]
+
+// The Artifactory thread pool, necessary to download metadata files as a
+// background task
+threadPool = ctx.beanForType(CachedThreadPoolTaskExecutor)
 
 download {
     // If we're getting from remote a repomd.xml file with legacy repodata file
-    // names, change those names to the modern style.
+    // names, change those names to the modern style, and cache these files.
     altRemoteContent { repoPath ->
         // don't modify the content unless we're dealing with a repomd.xml
-        def rpath = repoPath.path
-        if (!(rpath ==~ '^(?:.+/)?repodata/repomd\\.xml$')) return
-        def conn = null, istream = null
+        if (!(repoPath.path ==~ '^(?:.+/)?repodata/repomd\\.xml$')) return
+        def repoService = ctx.beanForType(InternalRepositoryService)
+        def repo = repoService.remoteRepositoryByKey(repoPath.repoKey)
+        def streamhandle = null, xml = null
         try {
-            def conf = repositories.getRepositoryConfiguration(repoPath.repoKey)
-            // get the url of the remote repo
-            def url = conf.url
-            if (!url.endsWith('/')) url += '/'
-            url += rpath
-            // get the remote authorization data
-            def auth = "$conf.username:$conf.password".bytes.encodeBase64()
-            // open a connection to the remote
-            conn = new URL(url).openConnection()
-            conn.setRequestProperty('Authorization', "Basic $auth")
-            // make sure the remote sends the correct response
-            def response = conn.responseCode
-            if (response < 200 || response >= 300) return
-            // parse the repomd.xml file
-            istream = conn.inputStream
-            def xml = new XmlParser().parse(istream)
-            // add the checksums to the file names, if they aren't already there
-            xml.each {
-                // do nothing if this node is the wrong type
-                if (it.name() != 'data' && it.name()?.getLocalPart() != 'data')
-                    return
-                // do nothing if there is no file path
-                def path = it?.location?.@href
-                if (path == null || path.isEmpty()) return
-                // do nothing if the file path is not a legacy repodata path
-                def match = path[0] =~ '^((?:.+/)?repodata/)([^/]+)$'
-                if (!match) return
-                // do nothing if the checksum does not exist or is the wrong
-                // type
-                if (!(it?.checksum?.@type[0] ==~ '^sha1?$')) return
-                // if everything checks out, modify the file path
-                def checksum = it.checksum.text()
-                it.location.@href = "${match[0][1]}$checksum-${match[0][2]}"
+            // open a stream to the remote and parse the repomd.xml file
+            log.info("Downloading and parsing repomd.xml")
+            streamhandle = repo.downloadResource(repoPath.path)
+            xml = new XmlParser().parseText(streamhandle.inputStream.text)
+        } finally {
+            // close the input stream
+            streamhandle?.close()
+        }
+        if (!xml) return
+        // add the checksums to the file names, if they aren't already there
+        def downloads = []
+        xml.each {
+            // do nothing if this node is the wrong type
+            if (it.name() != 'data' && it.name()?.localPart != 'data') return
+            // do nothing if there is no file path
+            def path = it?.location?.@href
+            if (!path) return
+            // do nothing if the checksum does not exist or is the wrong type
+            if (!(it?.checksum?.@type[0] ==~ '^sha1?$')) return
+            def checksum = it.checksum.text()
+            // do nothing if the file path is not a legacy repodata path
+            def match = path[0] =~ '^((?:.+/)?repodata/)([^/]+)$'
+            if (!match || match[0][2].startsWith("$checksum-")) return
+            // if everything checks out, modify the file path
+            log.info("Found legacy repodata file '{}'", path[0])
+            def newname = "${match[0][1]}$checksum-${match[0][2]}"
+            it.location.@href = newname
+            // if the file already exists in the cache, we're done
+            def npath = RepoPathFactory.create(repoPath.repoKey, newname)
+            if (repositories.exists(npath)) return
+            // if the file doesn't exist, add it to the list to download
+            synchronized (downloadMutex) {
+                if (!downloadQueue[repo.key + ':' + path[0]]) {
+                    downloadQueue[repo.key + ':' + path[0]] = true
+                    downloads << [path[0], newname, match]
+                }
             }
+        }
+        // if all metadata files already exist in the cache, return the new
+        // repomd.xml
+        if (!downloads) {
+            log.info("Serving most recent metadata")
             // write the modified xml to a string
             def writer = new StringWriter()
             def printer = new XmlNodePrinter(new PrintWriter(writer))
@@ -70,98 +90,64 @@ download {
             // set the new string as the new file content
             size = newXml.length()
             inputStream = new ByteArrayInputStream(newXml.bytes)
-        } finally {
-            // close everything
-            istream?.close()
-            conn?.disconnect()
-        }
-    }
-
-    // If we're getting from remote a modern repodata file that doesn't exist,
-    // yet a legacy file with the same checksum does, return that file instead.
-    beforeDownloadRequest { request, repoPath ->
-        def rpath = repoPath.path, rkey = repoPath.repoKey
-        // don't modify the path unless we're in a remote repo
-        if (!repositories.remoteRepositories.contains(rkey)) return
-        // don't modify the path if the file is already in the cache
-        if (repositories.exists(RepoPathFactory.create("$rkey-cache", rpath)))
             return
-        // don't modify the path unless we're dealing with a repodata file
-        def match = rpath =~ '^(?:.+/)?repodata/([0-9a-f]+)-[^/]+$'
-        if (!match) return
-        // get the url of the remote repo
-        def conf = repositories.getRepositoryConfiguration(rkey)
-        def url = conf.url
-        if (!url.endsWith('/')) url += '/'
-        url += rpath
-        // get the remote authorization data
-        def auth = "$conf.username:$conf.password".bytes.encodeBase64()
-        def conn = null, istream = null, realchecksum = null
-        try {
-            // open a connection to the remote
-            conn = new URL(url).openConnection()
-            conn.setRequestMethod('HEAD')
-            conn.setRequestProperty('Authorization', "Basic $auth")
-            // don't modify the path if this file already exists on the far end
-            if (conn.responseCode != 404) return
-        } finally {
-            // close everything
-            istream?.close()
-            conn?.disconnect()
         }
-        // extract the checksum, and make a legacy version of the filename
-        def checksum = match[0][1]
-        def newPath = rpath - ~"$checksum-(?![^/]*/)"
-        // check if this file is in the cache already
-        def cachedPath = RepoPathFactory.create("$rkey-cache", newPath)
-        def exists = repositories.exists(cachedPath)
-        if (exists) {
-            def fileInfo = repositories.getFileInfo(cachedPath)
-            realchecksum = fileInfo.checksumsInfo.sha1
-            // if the legacy file is cached, but it has the wrong checksum,
-            // expire it from the cache
-            if (checksum != realchecksum) {
-                expired = true
-                exists = false
-            }
-        }
-        // if the legacy file is not cached or was expired by the previous
-        // operation, attempt to pull the checksum from the far end
-        if (!exists) {
-            // get the url of the remote repo
-            url = conf.url
-            if (!url.endsWith('/')) url += '/'
-            url += newPath
-            // get the remote authorization data
-            conn = null
-            istream = null
-            try {
-                // open a connection to the remote
-                conn = new URL(url).openConnection()
-                conn.setRequestProperty('Authorization', "Basic $auth")
-                // don't modify the path if the source file does not exist
-                def response = conn.responseCode
-                if (response < 200 || response >= 300) return
-                // calculate the checksum of the response data
-                istream = conn.inputStream
-                def digest = MessageDigest.getInstance('SHA1')
-                def buf = new byte[4096]
-                def len = istream.read(buf)
-                while (len != -1) {
-                    digest.update(buf, 0, len)
-                    len = istream.read(buf)
+        // in separate threads, cache any necessary metadata files
+        downloads.each { download ->
+            threadPool.submit {
+                def (path, newname, match) = download
+                // download the new file
+                log.info("Repodata file '{}' not cached, downloading", newname)
+                def opath = RepoPathFactory.create(repoPath.repoKey, path)
+                def ctx = new NullRequestContext(opath)
+                def res = repo.getInfo(ctx)
+                repo.getResourceStreamHandle(ctx, res)?.close()
+                // move the file to the appropriate location
+                def cachekey = repo.localCacheRepo.key
+                def truesha = repositories.getFileInfo(opath).checksumsInfo.sha1
+                def truename = "${match[0][1]}$truesha-${match[0][2]}"
+                def truepath = RepoPathFactory.create(cachekey, truename)
+                def oldpath = RepoPathFactory.create(cachekey, path)
+                def status = new MoveMultiStatusHolder()
+                def config = new MoverConfigBuilder(oldpath, truepath)
+                config.failFast(true).atomic(true)
+                def mover = new DefaultRepoPathMover(status, config.build())
+                def mvsrc = repo.localCacheRepo.getImmutableFsItem(oldpath)
+                def mvdst = repoService.getRepoRepoPath(truepath)
+                mover.moveOrCopyMultiTx(mvsrc, mvdst)
+                log.info("Completed download of '{}'", truename)
+                synchronized (downloadMutex) {
+                    downloadQueue.remove(repo.key + ':' + path)
                 }
-                realchecksum = digest.digest().encodeHex().toString()
-            } finally {
-                // close everything
-                istream?.close()
-                conn?.disconnect()
             }
         }
-        // don't modify the path if the requested checksum differs from that of
-        // the source file
-        if (checksum != realchecksum) return
-        // if everything checks out, modify the path
-        modifiedRepoPath = RepoPathFactory.create(rkey, newPath)
+        // if there is no cached repomd.xml, return the new one
+        if (!repositories.exists(repoPath)) {
+            log.info("Serving most recent metadata")
+            log.warn("Some metadata files may 404 until they're fully cached")
+            // write the modified xml to a string
+            def writer = new StringWriter()
+            def printer = new XmlNodePrinter(new PrintWriter(writer))
+            printer.preserveWhitespace = true
+            printer.print(xml)
+            def newXml = writer.toString()
+            // set the new string as the new file content
+            size = newXml.length()
+            inputStream = new ByteArrayInputStream(newXml.bytes)
+            return
+        }
+        // just return the old repomd.xml
+        log.info("Serving old cached metadata")
+        def stream = null, oldxml = null
+        try {
+            // get the contents of the old repomd.xml
+            stream = repositories.getContent(repoPath)
+            oldxml = stream.inputStream.bytes
+        } finally {
+            // close the input stream
+            stream?.close()
+        }
+        size = oldxml.length
+        inputStream = new ByteArrayInputStream(oldxml)
     }
 }
