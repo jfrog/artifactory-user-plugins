c965580eda9cee64e3dcb4158344725c67c1424b
edwardl <edwardl@jfrog.com>
2016-10-11 14:09:46 -0700

    adding put and get security replication list
    Change-Id: I1742f4bd06a303b7f724da627e8849adfca94afc

---
+++ security/securityReplication/securityReplication.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.artifactory.resource.ResourceStreamHandle
+
+// usage: curl -X PUT http://localhost:8081/artifactory/api/plugins/execute/securityReplication
+
+executions {
+  def artHome = ctx.artifactoryHome.haAwareEtcDir
+
+  securityReplication(httpMethod: 'PUT') { ResourceStreamHandle body ->
+    def content = body.inputStream.text
+    def targetFile = new File(artHome, '/plugins/securityReplication.txt')
+    def w = targetFile.newWriter()
+    targetFile << content
+    w.close()
+  }
+  
+  securityReplicationList(httpMethod: 'GET') {
+    def content = new File(artHome, '/plugins/securityReplication.txt')
+    if (!content || !content.exists()){
+      message = "Error the security replication file either does not exist or contains no contents"
+      status = 400
+    } else {
+      message = content.text
+      status = 200
+    }
+  } 
+}
