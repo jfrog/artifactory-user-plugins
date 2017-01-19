a701b95f9dfc22d84d53fe589f0c956d0a1c929b
travisf <travisf@jfrog.com>
2017-01-19 12:39:25 -0800

    Added requestRouting test
    Change-Id: I8c04e29abc5113aaaf874343ec076735bf6df475

--- ha/requestRouting/RequestRoutingTest.groovy
+++ ha/requestRouting/RequestRoutingTest.groovy
@@ -1,19 +1,35 @@
 import spock.lang.Specification
+import groovy.json.JsonSlurper
 
+import org.jfrog.artifactory.client.ArtifactoryRequest
+import org.jfrog.artifactory.client.impl.ArtifactoryRequestImpl
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 
 class RequestRoutingTest extends Specification {
-    def 'test name'() {
-        setup:
-        def baseurl = 'http://localhost:8088/artifactory'
-        def artifactory = create(baseurl, 'admin', 'password')
-
+    def 'request routing test'() {
         when:
-        null
+        def baseurlx = "http://localhost:$portx/artifactory"
+        def artx = create(baseurlx, 'admin', 'password')
+        def baseurly = "http://localhost:$porty/artifactory"
+        def arty = create(baseurly, 'admin', 'password')
+        def pluginpath = "api/plugins/execute/getVersion"
+        def pluginreq = new ArtifactoryRequestImpl().apiUrl(pluginpath)
+        pluginreq.addQueryParam("params", "serverId=$id")
+        pluginreq.method(ArtifactoryRequest.Method.GET)
+        pluginreq.responseType(ArtifactoryRequest.ContentType.TEXT)
+        def pluginstream = artx.restCall(pluginreq)
+        def pluginout = new JsonSlurper().parseText(pluginstream)
+        def apireq = new ArtifactoryRequestImpl().apiUrl("api/system/version")
+        apireq.method(ArtifactoryRequest.Method.GET)
+        apireq.responseType(ArtifactoryRequest.ContentType.JSON)
+        def apiout = arty.restCall(apireq)
 
         then:
-        false
+        pluginout == apiout
 
-        //cleanup:
+        where:
+        id    << ["art2", "art1"]
+        portx << ["8088", "8081"]
+        porty << ["8081", "8088"]
     }
 }
