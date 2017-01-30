29c9e0743f728d9b66970bfffaa1f6582f23885b
travisf <travisf@jfrog.com>
2017-01-30 11:46:27 -0800

    Added test for Whitesource
    Change-Id: I57c7766757549d374aca473e3dcbf18dec6f0bc1

--- governance/whitesource/setup.groovy
+++ governance/whitesource/setup.groovy
@@ -1,5 +1,5 @@
 // install dependency libraries
-lib1 = 'org/whitesource/wss-agent-api/2.2.9/wss-agent-api-2.2.9.jar'
-lib2 = 'org/whitesource/wss-agent-api-client/2.2.9/wss-agent-api-client-2.2.9.jar'
-lib3 = 'org/whitesource/wss-agent-report/2.2.9/wss-agent-report-2.2.9.jar'
+lib1 = 'org/whitesource/wss-agent-api/2.3.1/wss-agent-api-2.3.1.jar'
+lib2 = 'org/whitesource/wss-agent-api-client/2.3.1/wss-agent-api-client-2.3.1.jar'
+lib3 = 'org/whitesource/wss-agent-report/2.3.1/wss-agent-report-2.3.1.jar'
 installLibraries([lib1, lib2, lib3])
