667a061f9e071669f4c65fa524351d7ce115ed89
Rasika <rasikai@jfrog.com>
2017-02-13 10:18:39 -0800

    Added test for delegateAuthenticationRealm
    Change-Id: I55abddb76e15b4da39c269c28f6018f06cae8940

--- security/delegateAuthenticationRealm/DelegateAuthenticationRealmTest.groovy
+++ security/delegateAuthenticationRealm/DelegateAuthenticationRealmTest.groovy
@@ -1,17 +1,48 @@
 import spock.lang.Specification
+import org.artifactory.security.User
 
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.model.builder.UserBuilder
 
 class DelegateAuthenticationRealmTest extends Specification {
-    def 'test name'() {
+    def 'delegate authentication realm test'() {
         setup:
-        def baseurl = 'http://localhost:8088/artifactory'
-        def artifactory = create(baseurl, 'admin', 'password')
+        def baseurl1 = 'http://localhost:8081/artifactory'
+        def baseurl2 = 'http://localhost:8088/artifactory'
+        def artifactory1 = create(baseurl1, 'admin', 'password')
+        def artifactory2 = create(baseurl2, 'admin', 'password')
 
-        // when:
+        when:
+        UserBuilder userBuilder = artifactory1.security().builders().userBuilder()
+        def user1 = userBuilder.name("newuser")
+        .email("newuser@jfrog.com")
+        .admin(true)
+        .profileUpdatable(true)
+        .password("password")
+        .build();
+        artifactory1.security().createOrUpdate(user1)
 
-        // then:
+        def auth = "Basic ${'newuser:password'.bytes.encodeBase64()}"
+        def conn = new URL("${baseurl2}/api/system").openConnection()
+        conn.requestMethod = 'GET'
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        conn.disconnect()
 
-        // cleanup:
+        then:
+        def user2 = artifactory2.security().user("newuser")
+        String name = user2.getName()
+        String email = user2.getEmail()
+        boolean profileUpdatable = user2.isProfileUpdatable()
+        boolean isAdmin = user2.isAdmin()
+
+        name == "newuser"
+        email == "newuser@jfrog.com"
+        isAdmin == true
+        profileUpdatable == true
+
+        cleanup:
+        String result1 = artifactory1.security().deleteUser("newuser")
+        String result2 = artifactory2.security().deleteUser("newuser")
     }
 }
--- security/delegateAuthenticationRealm/delegateAuthenticationRealm.groovy
+++ security/delegateAuthenticationRealm/delegateAuthenticationRealm.groovy
@@ -33,8 +33,8 @@ import org.artifactory.security.User
  * @since 02/04/15
  */
 
-@Field final String centralAuthenticatorArtifactory = 'http://localhost:8081/artifactory'
-@Field final String uriPath = '/api/plugins/execute/getCurrentUserDetails'
+@Field final String centralAuthenticatorArtifactory = 'http://localhost:8081'
+@Field final String uriPath = '/artifactory/api/plugins/execute/getCurrentUserDetails'
 
 realms {
     delegateRealm(autoCreateUsers: true) {
@@ -66,6 +66,8 @@ realms {
                         currentUser.email = json.email
                         currentUser.groups = json.groups
                         currentUser.setUserProperty("basictoken", json?.userProperties?.basictoken)
+                        currentUser.admin = json.admin
+                        currentUser.updatableProfile = json.updatableProfile
                         log.info "User ${currentUser.username} was successfully synchronize"
                         passed = true
                     }
