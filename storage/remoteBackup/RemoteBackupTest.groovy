92e57bdfdb14c865542322228a67303d45aebf3d
travisf <travisf@jfrog.com>
2017-02-24 16:02:03 -0800

    Added remoteBackup plugin
    Added a plugin that automatically backs up remote caches.
    
    Change-Id: I80173d59eb93fe6ab939c23172d00b2c949e3b9a

---
+++ storage/remoteBackup/RemoteBackupTest.groovy
@@ -0,0 +1,42 @@
+import spock.lang.Specification
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class RemoteBackupTest extends Specification {
+    def 'remote backup test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        def builder = artifactory.repositories().builders()
+        def localrepo = null, remoterepo = null
+
+        when:
+        def remote = builder.remoteRepositoryBuilder().key('backup-remote')
+        remote.repositorySettings(new MavenRepositorySettingsImpl())
+        remote.url('https://jcenter.bintray.com')
+        artifactory.repositories().create(0, remote.build())
+        remoterepo = artifactory.repository('backup-remote')
+        def remotepath1 = 'junit/junit/3.8.1/junit-3.8.1.pom'
+        remoterepo.download(remotepath1).doDownload().text
+        def local = builder.localRepositoryBuilder().key('backup-local')
+        local.repositorySettings(new MavenRepositorySettingsImpl())
+        artifactory.repositories().create(0, local.build())
+        localrepo = artifactory.repository('backup-local')
+        def remotepath2 = 'xmlpull/xmlpull/1.1.3.1/xmlpull-1.1.3.1.pom'
+        remoterepo.download(remotepath2).doDownload().text
+
+        then:
+        localrepo.file(remotepath2).info()
+
+        when:
+        artifactory.plugins().execute('remoteBackup').sync()
+
+        then:
+        localrepo.file(remotepath1).info()
+
+        cleanup:
+        remoterepo?.delete()
+        localrepo?.delete()
+    }
+}
---
+++ storage/remoteBackup/remoteBackup.groovy
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2017 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+
+import org.artifactory.repo.RepoPathFactory
+
+// A cron expression describing how often to run the backup. Modify this
+// variable to customize the timing.
+cronExpression = "0 0 0/1 * * ?"
+
+jobs {
+  remoteBackupJob(cron: cronExpression) {
+    runBackup(null)
+  }
+}
+
+executions {
+  remoteBackup() { params ->
+    def repos = params?.getAt('repos') ?: null
+    def (complete, total) = runBackup(repos)
+    if (complete instanceof CharSequence) {
+      message = "Error copying items: $complete"
+      status = 400
+    } else if (complete < total) {
+      message = "Successfully copied $complete of $total items"
+    } else {
+      message = "Successfully copied all $total items"
+    }
+  }
+}
+
+storage {
+  afterCreate { item ->
+    def etcdir = ctx.artifactoryHome.haAwareEtcDir
+    def cfgfile = new File(etcdir, "plugins/remoteBackup.json")
+    def cfg = new JsonSlurper().parse(cfgfile)
+    if (item.repoKey in cfg && !item.isFolder()) {
+      asSystem {
+        def dest = cfg[item.repoKey]
+        try {
+          def destpath = RepoPathFactory.create(dest, item.relPath)
+          repositories.copy(item.repoPath, destpath)
+        } catch (Exception ex) {
+          log.warn("Unable to backup $item.repoPath to $dest: $ex.message")
+        }
+      }
+    }
+  }
+}
+
+def runBackup(repos) {
+  def etcdir = ctx.artifactoryHome.haAwareEtcDir
+  def cfgfile = new File(etcdir, "plugins/remoteBackup.json")
+  def cfg = new JsonSlurper().parse(cfgfile)
+  if (repos) {
+    def cfgtmp = [:]
+    for (repo in repos) {
+      if (repo in cfg) cfgtmp[repo] = cfg[repo]
+      else return ["Repo $repo not in configuration"]
+    }
+    cfg = cfgtmp
+  }
+  def complete = 0, total = 0
+  for (repopair in cfg.entrySet()) {
+    def src = repopair.key, dest = repopair.value
+    def quer = new JsonBuilder([type: 'file', repo: src]).toString()
+    def aql = "items.find($quer).include(\"repo\",\"path\",\"name\")"
+    searches.aql(aql) {
+      for (item in it) {
+        def path = item.path + '/' + item.name
+        def srcpath = RepoPathFactory.create(src, path)
+        def destpath = RepoPathFactory.create(dest, path)
+        if (repositories.exists(srcpath) &&
+            (!repositories.exists(destpath) ||
+             (repositories.getFileInfo(srcpath).checksumsInfo.sha1 !=
+              repositories.getFileInfo(destpath).checksumsInfo.sha1))) {
+          total += 1
+          try {
+            repositories.copy(srcpath, destpath)
+            complete += 1
+          } catch (Exception ex) {
+            log.warn("Unable to backup $srcpath to $dest: $ex.message")
+          }
+        }
+      }
+    }
+  }
+  return [complete, total]
+}
