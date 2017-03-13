889b6ba8414dc1209abf6f294ccb9410ae5c8504
Rasika <rasikai@jfrog.com>
2017-03-13 11:54:05 -0700

    Updated test for layoutProperties
    Change-Id: I2586a04ccfb8361cf83839173dc74fc81aa17913

--- storage/layoutProperties/LayoutPropertiesTest.groovy
+++ storage/layoutProperties/LayoutPropertiesTest.groovy
@@ -1,14 +1,22 @@
 import spock.lang.Specification
 
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 
 class LayoutPropertiesTest extends Specification {
     def 'maven layout properties plugin test'() {
         setup:
-        def repo = 'libs-release-local'
-        def path = 'org/test/modname/1.0/modname-1.0.txt'
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
+
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+
+        def path = 'org/test/modname/1.0/modname-1.0.txt'
+        def repo = 'maven-local'
 
         when:
         def stream = new ByteArrayInputStream('test'.getBytes('utf-8'))
@@ -21,6 +29,6 @@ class LayoutPropertiesTest extends Specification {
         props['layout.baseRevision'] == ['1.0']
 
         cleanup:
-        artifactory.repository(repo).delete('org')
+        artifactory.repository(repo)
     }
 }
