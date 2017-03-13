154ab1a733242d99214c04fe8fb31d1b94ee4ba1
Rasika <rasikai@jfrog.com>
2017-03-13 11:47:14 -0700

    Updated test for archiveOldArtifacts
    Change-Id: If34034738e1e4580fbefb21d14b71a91e5db60d9

--- archive/archiveOldArtifacts/ArchiveOldArtifactsTest.groovy
+++ archive/archiveOldArtifacts/ArchiveOldArtifactsTest.groovy
@@ -1,5 +1,7 @@
 import spock.lang.Specification
 
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 
 class ArchiveOldArtifactsTest extends Specification {
@@ -7,19 +9,30 @@ class ArchiveOldArtifactsTest extends Specification {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
+
+        def builder = artifactory.repositories().builders()
+        def local1 = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local1)
+
+        def local2 = builder.localRepositoryBuilder().key('maven-archive')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local2)
+
         def stream = new ByteArrayInputStream('test'.getBytes('utf-8'))
-        artifactory.repository('libs-release-local').upload('foo.txt', stream).doUpload()
-        artifactory.repository('libs-release-local').file('foo.txt').properties().addProperty('archive', 'yes').doSet()
+        artifactory.repository('maven-local').upload('foo.txt', stream).doUpload()
+        artifactory.repository('maven-local').file('foo.txt').properties().addProperty('archive', 'yes').doSet()
 
         when:
         def pt1 = "http://localhost:8088/artifactory/api/plugins/execute/archive_old_artifacts"
-        def pt2 = "params=includePropertySet=archive|srcRepo=libs-release-local|archiveRepo=plugins-release-local"
+        def pt2 = "params=includePropertySet=archive%7CsrcRepo=maven-local%7CarchiveRepo=maven-archive"
         "curl -X POST -uadmin:password $pt1?$pt2".execute().waitFor()
 
         then:
-        artifactory.repository('plugins-release-local').file('foo.txt').getPropertyValues('archived.timestamp')
+        artifactory.repository('maven-archive').file('foo.txt').getPropertyValues('archived.timestamp')
 
         cleanup:
-        artifactory.repository('plugins-release-local').delete('foo.txt')
+        artifactory.repository('maven-local').delete()
+        artifactory.repository('maven-archive').delete()
     }
 }
