b64faeaac7232e1019cefc901ebdc96a34205d33
Rasika <rasikai@jfrog.com>
2017-03-27 10:50:39 -0700

    Updated test for repoQuota
    Change-Id: Id89328daac6b030412f7f5f46c56c292298c9d24

--- storage/repoQuota/RepoQuotaTest.groovy
+++ storage/repoQuota/RepoQuotaTest.groovy
@@ -1,20 +1,36 @@
 import spock.lang.Specification
+import groovyx.net.http.HttpResponseException
 
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
 
 class RepoQuotaTest extends Specification {
-    def 'test name'() {
+    def 'repo quota test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
 
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        def conn = new URL("${baseurl}/api/storage/maven-local?properties=repository.path.quota=5").openConnection()
+        conn.requestMethod = 'PUT'
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 204
+        conn.disconnect()
+
         when:
-        null
+        artifactory.repository('maven-local').upload('file1', new ByteArrayInputStream('test1'.getBytes('utf-8'))).doUpload()
+        artifactory.repository('maven-local').upload('file2', new ByteArrayInputStream('test2'.getBytes('utf-8'))).doUpload()
 
         then:
-        false
+        thrown(HttpResponseException)
+        artifactory.repository('maven-local').file('file1').info()
 
-        //cleanup:
+        cleanup:
+        artifactory.repository("maven-local").delete()
     }
 }
-
