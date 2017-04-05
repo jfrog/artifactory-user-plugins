eb24d504dde5327941236d920846599ca65d45dc
Rasika <rasikai@jfrog.com>
2017-04-05 12:18:02 -0700

    Updated README and test for newFileWorkFlow

--- storage/newFileWorkflow/NewFileWorkflowTest.groovy
+++ storage/newFileWorkflow/NewFileWorkflowTest.groovy
@@ -1,19 +1,50 @@
 import spock.lang.Specification
+import groovy.json.JsonSlurper
 
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
 
 class NewFileWorkflowTest extends Specification {
-    def 'test name'() {
+    def 'new file workflow test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
+        
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
 
         when:
-        null
+        artifactory.repository('maven-local').upload('pass', new ByteArrayInputStream('B'.getBytes('utf-8'))).doUpload()
+        sleep(21000)
+
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        def conn = new URL("${baseurl}/api/storage/maven-local/pass?properties").openConnection()
+        conn.requestMethod = 'GET'
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def json = new JsonSlurper().parse(conn.getInputStream())
+        conn.disconnect()
 
         then:
-        false
+        json.properties['workflow.status'] == ['EXECUTED']
 
-        //cleanup:
+        when:
+        artifactory.repository('maven-local').upload('fail', new ByteArrayInputStream('A'.getBytes('utf-8'))).doUpload()
+        sleep(21000)
+
+        conn = new URL("${baseurl}/api/storage/maven-local/fail?properties").openConnection()
+        conn.requestMethod = 'GET'
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        json = new JsonSlurper().parse(conn.getInputStream())
+        conn.disconnect()
+
+        then:
+        json.properties['workflow.status'] == ['FAILED_EXECUTION']
+
+        cleanup:
+        artifactory.repository('maven-local').delete()
     }
 }
--- storage/newFileWorkflow/README.md
+++ storage/newFileWorkflow/README.md
@@ -1,1 +1,16 @@
-*This plugin is currently being tested for Artifactory 5.x releases.*
+Artifactory New File Workflow Plugin
+=============================================
+
+This plugin checks newly uploaded artifacts for something specific defined in `dummyExecute`. Before a new artifact has been checked, the property `workflow.status` is set to `NEW`. While the new artifact is being checked, the property is changed to ```PENDING```. Then, the property is set to `EXECUTED` or `FAILED_EXECUTION` depending on what the artifact was checked for. The plugin executes every 20 seconds.
+
+The default function `dummyExecute` checks whether the file contains an `'A'`. The property `workflow.status` is set to `EXECUTED` if the file did not have an A in it, and `FAILED_EXECUTION` if the file contained an A.
+
+Installation
+------------
+
+To install this plugin:
+
+1. Place this script under the master Artifactory server
+   `${ARTIFACTORY_HOME}/etc/plugins`.
+2. Verify in the `${ARTIFACTORY_HOME}/logs/artifactory.log` that the plugin loaded
+   correctly.
