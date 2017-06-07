096099da294eaf30d24bd098251601031b75d15a
Rasika <rasikai@jfrog.com>
2017-06-07 14:24:03 -0700

    Updated test for exposeFilestore

--- filestore/exposeFilestore/ExposeFilestoreTest.groovy
+++ filestore/exposeFilestore/ExposeFilestoreTest.groovy
@@ -1,19 +1,39 @@
 import spock.lang.Specification
-
+import java.nio.file.Files
+import java.nio.file.Paths
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
 
 class ExposeFilestoreTest extends Specification {
-    def 'test name'() {
+    def 'expose filestore test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64().toString()}"
         def artifactory = create(baseurl, 'admin', 'password')
+        
+
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+
+        artifactory.repository('maven-local').upload('foo/bar/file', new ByteArrayInputStream('test'.getBytes('utf-8')))
+        .withProperty("expose", "true")
+        .doUpload()
+
 
         when:
-        null
+        def conn = new URL(baseurl + '/api/plugins/execute/exposeRepository?params=repo=maven-local%7Cdest=/tmp').openConnection()
+        conn.setRequestMethod('POST')
+        conn.doOutput = true
+        conn.setRequestProperty('Authorization', auth)
+        conn.setRequestProperty('Content-Type', 'application/json')
+        conn.getResponseCode()
 
         then:
-        false
+        Files.isSymbolicLink(Paths.get('/tmp/foo/bar/file'))
 
-        //cleanup:
+        cleanup:
+        artifactory.repository('maven-local').delete()
     }
 }
