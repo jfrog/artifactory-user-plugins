db2d611dc6d1a116e16e4c82bd0cc85f32c4a73a
Rasika <rasikai@jfrog.com>
2017-06-07 17:24:44 -0700

    Updated test for getUsersByGroup

--- security/getUsersByGroup/GetUsersByGroupTest.groovy
+++ security/getUsersByGroup/GetUsersByGroupTest.groovy
@@ -1,19 +1,45 @@
 import spock.lang.Specification
-
+import org.jfrog.artifactory.client.model.Group
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.model.builder.UserBuilder
 
 class GetUsersByGroupTest extends Specification {
-    def 'test name'() {
+    def 'get users by group test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
 
         when:
-        null
+        UserBuilder userBuilder = artifactory.security().builders().userBuilder()
+        def user = userBuilder.name("user")
+        .email("user@jfrog.com")
+        .admin(true)
+        .profileUpdatable(true)
+        .password("password")
+        .build();
+        artifactory.security().createOrUpdate(user)
+
+        Group group = artifactory.security().builders().groupBuilder()
+        .name("group")
+        .autoJoin(true)
+        .description("new group")
+        .build();
+        artifactory.security().createOrUpdateGroup(group);
+
+        def conn = new URL(baseurl + '/api/plugins/execute/getUsersByGroup?params=group=group').openConnection()
+        conn.setRequestMethod('GET')
+        conn.doOutput = true
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def output = conn.getInputStream().text
+        conn.disconnect()
 
         then:
-        false
+        output.contains('\"group\"')
 
-        //cleanup:
+        cleanup:
+        String result = artifactory.security().deleteUser("user")
+        artifactory.security().deleteGroup("group")
     }
 }
