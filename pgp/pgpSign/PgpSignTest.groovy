c45841ada37560341047e1a4c3802cc1ed573082
Rasika <rasikai@jfrog.com>
2017-06-23 16:07:05 -0700

    Updated test for PGPSign

--- pgp/pgpSign/PgpSignTest.groovy
+++ pgp/pgpSign/PgpSignTest.groovy
@@ -1,19 +1,29 @@
 import spock.lang.Specification
-
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import groovyx.net.http.HttpResponseException
 
 class PgpSignTest extends Specification {
-    def 'test name'() {
+    def 'pgp sign test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
 
+        def builder = artifactory.repositories().builders()
+        def repository = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, repository)
+
+        def file = new ByteArrayInputStream('test'.bytes)
+        artifactory.repository("maven-local").upload('test', file).doUpload()
+
         when:
-        null
+        artifactory.repository("maven-local").file('test.asc').info()
 
         then:
-        false
+        notThrown(HttpResponseException)
 
-        //cleanup:
+        cleanup:
+        artifactory.repository("maven-local").delete()
     }
 }
