0f7450c95c0816a7b5f7071042e07476a189fa1e
Elio Marcolino <eliom@jfrog.com>
2017-07-21 16:32:45 -0700

    Removing unecessary prints to console

--- backup/backupFolders/BackupFoldersTest.groovy
+++ backup/backupFolders/BackupFoldersTest.groovy
@@ -5,6 +5,21 @@ import spock.lang.Specification
 
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 
+/*
+ * Copyright (C) 2017 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 class BackupFoldersTest extends Specification {
     static final baseurl = 'http://localhost:8088/artifactory'
     static final user = 'admin'
@@ -111,7 +126,6 @@ class BackupFoldersTest extends Specification {
 
     def isFilePresentInBackup(backupFolder, repoKey, path) {
         def filePath = "${backupFolder.absolutePath}/${backupFolder.list()[0]}/$repoKey/$path"
-        println filePath
         return new File(filePath).exists()
     }
 
