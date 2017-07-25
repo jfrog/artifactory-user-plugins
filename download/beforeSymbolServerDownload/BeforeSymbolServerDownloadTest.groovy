e111f80c53aa557f6dbaf18298ee211a92979540
David Shin <davids@jfrog.com>
2017-07-25 11:07:31 -0700

    update test script and README

--- download/beforeSymbolServerDownload/BeforeSymbolServerDownloadTest.groovy
+++ download/beforeSymbolServerDownload/BeforeSymbolServerDownloadTest.groovy
@@ -1,19 +1,61 @@
 import spock.lang.Specification
-
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.model.repository.settings.impl.NugetRepositorySettingsImpl
+import org.apache.http.client.methods.*
+import org.artifactory.repo.*
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.HttpResponseException
+
+import javax.xml.ws.http.HTTPBinding
+import static groovyx.net.http.Method.GET
+import static groovyx.net.http.ContentType.TEXT
+
 
 class BeforeSymbolServerDownloadTest extends Specification {
-    def 'test name'() {
+
+    static final user = 'admin'
+    static final password='password'
+    static final userPassword="$user:$password"
+    static final auth = "Basic ${userPassword.bytes.encodeBase64()}"
+    static final url = "http://msdl.microsoft.com/download/symbols"
+    static final filePath ='wininet.pdb/7CE26DE332694328B6EB5F3C69DB20CC2/wininet.pd_'
+    def repokey = 'microsoft-symbols'
+
+    def 'before Symbol Server download test '() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
+	def builder = artifactory.repositories().builders()
 
-        when:
-        null
+	def remoteRepo= builder.remoteRepositoryBuilder().key(repokey).url(url).repositorySettings(new NugetRepositorySettingsImpl()).build()
+	artifactory.repositories().create(0,remoteRepo)
+	
+	when:
+	def http = new HTTPBuilder("${baseurl}/${repokey}/${filePath}")
+	http.setHeaders([Authorization: "${auth}"]) 
 
-        then:
-        false
+	then:
+        def verified = http.request(GET, TEXT) { req ->
+            response.success = { resp, inputStream ->
+            }
+	 }
+	sleep(5000)
 
-        //cleanup:
+        when :
+	def logfile ='http://localhost:8088/logs/artifactory.log'
+  	def conn 
+        conn = new URL (logfile).openConnection()
+        conn.requestMethod = 'GET'
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def reader = new InputStreamReader(conn.inputStream)
+        def textlog = reader.text
+        conn.disconnect()
+	
+	then:
+	assert textlog.contains("User-Agent: Microsoft-Symbol-Server/6.3.9600.17095")==true
+ 
+        cleanup:
+	artifactory.repository(repokey).delete()
     }
 }
--- download/beforeSymbolServerDownload/README.md
+++ download/beforeSymbolServerDownload/README.md
@@ -1,8 +1,25 @@
-Artifactory Before Symbol Server User Plugin
-============================================
-
-*This plugin is currently being tested for Artifactory 5.x releases.*
-
+﻿Artifactory Before Symbol Server Download User Plugin
+This plugin is currently being tested for Artifactory 4.x and 5.x releases.
 The Artifactory Symbol Server Plugin listens for requests for symbol files and then redirects them to the Microsoft Symbol Server.
-
 For more information on how to use this plugin, please go to: https://www.jfrog.com/confluence/display/RTF/Microsoft+Symbol+Server
+Features
+This plugin adds “User-Agent: Microsoft-Symbol-Server” to http.headers before fetching a symbol file from the Microsoft Symbol Server.
+Installation
+To install this plugin:
+1. Place file beforeSymbolServerDownload.groovy under the Artifactory server ${ARTIFACTORY_HOME}/etc/plugins.
+2. Verify in the ${ARTIFACTORY_HOME}/logs/artifactory.log that the plugin loaded correctly.
+
+
+To enable logging for the plugin, add the below logger to your ${ARTIFACTORY.HOME}/etc/ logback.xml:
+   <logger name="beforeSymbolServerDownload">
+        <level value="debug"/>
+   </logger>
+   <!-- This logger is necessary for test script running so as to check http-outgoing value of   
+    request.headers  -->
+   <logger name="org.apache.http.wire">  
+      <level value="debug"/>
+   </logger>
+
+
+To enable downloading http://localhost:port/logs/artifactory.log for the test purpose, you need to make logs.xml file as below under ${ARTIFACTORY-HOME}/tomcat/conf/Catalina/localhost/  
+    <Context override="true" path="/logs" docBase="${artifactory.home}/logs" ></Context>
