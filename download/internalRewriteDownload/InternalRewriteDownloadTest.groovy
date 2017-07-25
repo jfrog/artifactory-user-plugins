e87cdf83a909001204c63e4b025d32735b725143
Mari Yamaguchi <mariy@ip-10-6-18-143.eu-west-1.compute.internal>
2017-07-25 16:17:07 -0700

    changes in test script and README

--- download/internalRewriteDownload/InternalRewriteDownloadTest.groovy
+++ download/internalRewriteDownload/InternalRewriteDownloadTest.groovy
@@ -2,15 +2,12 @@ import spock.lang.Specification
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 import org.jfrog.artifactory.client.model.repository.settings.impl.GenericRepositorySettingsImpl
 
-
 class InternalRewriteDownloadTest extends Specification {
 
     def 'test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
         def artifactory = create(baseurl, 'admin', 'password')
-         
-
         
         //building dist-local repo
         def builder = artifactory.repositories().builders()
@@ -20,37 +17,22 @@ class InternalRewriteDownloadTest extends Specification {
 
         def repo = artifactory.repository("dist-local")
 
-
         def myVersion = "VERSION" 
         myVersion = "latest"
         
-        //uploading first file
-        def path ="$myVersion/test1.txt"
-        def file = new File('./src/test/groovy/InternalRewriteDownloadTest/test1.txt')
-        file.write "$myVersion"        
-        repo.upload(path,file).doUpload();
-            
-        //uploading the file a second time 
-        myVersion = "1.0"
-        def path2 = "$myVersion/test1.txt"
-        def file2 = new File('./src/test/groovy/InternalRewriteDownloadTest/test1.txt')
-        file.write "$myVersion"
-        repo.upload(path2,file2).doUpload();
+        //uploading test file
+        def path ="$myVersion/test1.txt"    
+        repo.upload(path, new ByteArrayInputStream('filecontents'.bytes)).doUpload()
 
         when: 
-        //adding the property 'latest.folderName' with the value '1.0' to the root (dist-local)
-        repo.file("/").properties().addProperty("latest.folderName","1.0").doSet()
+        //adding the property 'latest.folderName' with the value 'latest' to the root (dist-local)
+        repo.file("/").properties().addProperty("latest.folderName","latest").doSet()
 
         //downloading the most recent file from the 'latest' folder 
         repo.download("latest/test1.txt").doDownload();
 
         then:
-        //checking to see if the test passed via output in the console 
-        if(file2.readLines()[0]==myVersion) { 
-           System.out.println("test passed")  
-        } else{
+        assert path.readLines().contains('latest/test1.txt')
 
-            System.out.println("test failed")
-        }
     }
 }
