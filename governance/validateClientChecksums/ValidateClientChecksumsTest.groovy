dea04b5b343eedf0a560bba916cba5ed3da97572
Sara Ahbabou <saraa@jfrog.com>
2017-07-25 10:27:53 -0700

    Now uploading 2nd file from HTTP client"

--- governance/validateClientChecksums/ValidateClientChecksumsTest.groovy
+++ governance/validateClientChecksums/ValidateClientChecksumsTest.groovy
@@ -2,6 +2,9 @@ import spock.lang.Specification
 
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
 
+import java.net.HttpURLConnection
+import java.net.URL
+
 import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
 
 class ValidateClientChecksumsTest extends Specification {
@@ -13,6 +16,7 @@ class ValidateClientChecksumsTest extends Specification {
 
         def builder = artifactory.repositories().builders()
 
+        // create 1st repo
         def local = builder.localRepositoryBuilder().key('local-repo')
             
             local.repositorySettings(new MavenRepositorySettingsImpl())
@@ -24,11 +28,42 @@ class ValidateClientChecksumsTest extends Specification {
         
 
         def repo = artifactory.repository('local-repo')
-        def manual_file = artifactory.repository('maven-local')
-        def file1 = repo.upload("text.txt", new ByteArrayInputStream('helloworld'.bytes)).doUpload();    
+        // upload first file to first repo
+        def file1 = repo.upload("text.txt", new ByteArrayInputStream('helloworld'.bytes)).doUpload(); 
 
-        
-        def manual_local_repo = artifactory.repository('maven-local').file('pom.xml').info()
+        // create second repo
+        def manual_local = builder.localRepositoryBuilder().key('manual-repo')
+            
+            manual_local.repositorySettings(new MavenRepositorySettingsImpl())
+
+         
+            artifactory.repositories().create(0, manual_local.build())
+    
+
+        // upload second file to second repo t    
+        URL new_url = new URL("http://localhost:8088/artifactory/manual-repo/file.txt");
+        URLConnection newConnection = new_url.openConnection();
+
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        newConnection.setRequestProperty("Authorization", auth);
+
+
+        newConnection.setDoOutput(true);
+        newConnection.setRequestMethod("PUT");
+        // pass checksums as headers 
+        newConnection.setRequestProperty("X-Checksum-Sha1", "0bc2a0d661d056175b425e63d7aa3d0832d2a2ac");
+        newConnection.setRequestProperty("X-Checksum-Md5", "a2ed931ecace715a6e61fb0ca273480a");
+         OutputStreamWriter out = new OutputStreamWriter(
+            newConnection.getOutputStream());
+        out.write("Resource content");
+
+        out.close();
+        newConnection.getInputStream();
+
+
+
+        // get file info from first and second repo
+        def manual_local_repo = artifactory.repository('manual-repo').file('file.txt').info()
         def rest_local_repo = artifactory.repository('local-repo').file('text.txt').info()
 
          
@@ -36,14 +71,20 @@ class ValidateClientChecksumsTest extends Specification {
         def manual_file_checksum = manual_local_repo.getChecksums().getMd5()
 
         def rest_file_checksum  = rest_local_repo.getOriginalChecksums().getMd5()
+
+
         
       
         when: 
             
             try {
-            def manual_file_upload = manual_file.download('pom.xml').doDownload()
+
             def rest_file_upload = repo.download('text.txt').doDownload()
-                 }   
+            def manual_file_upload = manual_local.download('file.txt').doDownload()
+
+                 
+                 } 
+
             catch(HttpResponseException) {
                 if(HttpResponseException.getStatusCode() == 409) {
                     println "print failed because original checksum is null"
@@ -55,11 +96,11 @@ class ValidateClientChecksumsTest extends Specification {
          
         assert manual_file_checksum != null
         assert rest_file_checksum == null
-
-
         
         
-        cleanup:
+       cleanup:
         artifactory.repository('local-repo').delete()
+        artifactory.repository('manual-repo').delete()
+
     }
 }
