371fa09c4b20749c6f10522b5fb0d9e2f507f9d6
Scott Mosher <scottm@jfrog.com>
2017-08-02 12:05:25 -0700

    Update test file to user Artifactory Client instead of curl

--- build/removeModuleProperties/test/RemoveModulePropertiesTest.groovy
+++ build/removeModuleProperties/test/RemoveModulePropertiesTest.groovy
@@ -1,31 +1,37 @@
 import spock.lang.Specification
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.jfrog.artifactory.client.ArtifactoryRequest
+import org.jfrog.artifactory.client.impl.ArtifactoryRequestImpl
 import groovy.json.*
 
 
 class RemoveModulePropertiesTest extends Specification {
     def 'removeModulePropertiesTest'() {
         setup:
-        def slurper = new JsonSlurper()
-        def flag = true
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
 
-        ['bash','-c','curl -uadmin:password -X PUT http://localhost:8088/artifactory/api/build -H "Content-Type: application/json" --data-binary @./src/test/groovy/RemoveModulePropertiesTest/build.json'].execute()
-        ['bash','-c','mkdir /tmp/buildScript'].execute()
+        def file = new File('./src/test/groovy/RemoveModulePropertiesTest/build.json')
+
+        ArtifactoryRequest uploadBuild = new ArtifactoryRequestImpl().apiUrl("api/build")
+          .method(ArtifactoryRequest.Method.PUT)
+          .requestType(ArtifactoryRequest.ContentType.JSON)
+          .requestBody(new JsonSlurper().parse(file))
+        artifactory.restCall(uploadBuild)
 
         when:
-        ['bash','-c','curl -uadmin:password -X GET http://localhost:8088/artifactory/api/build/stm-test/46 > /tmp/buildScript/output.json'].execute().waitFor()
-        def buildFile = new File('/tmp/buildScript/output.json')
+        ArtifactoryRequest request = new ArtifactoryRequestImpl().apiUrl("api/build/stm-test/46")
+          .method(ArtifactoryRequest.Method.GET)
+          .responseType(ArtifactoryRequest.ContentType.JSON)
+        def response = artifactory.restCall(request)
 
         then:
-        def res = slurper.parseText(buildFile.getText())
-        res.buildInfo.modules.each{ m ->
-      	    if (m.properties != [:])
-              flag = false
-        }
-        flag
+        response.buildInfo.modules.every { it.properties == [:] }
 
         cleanup:
-        ['bash','-c','rm -r /tmp/buildScript'].execute()
-        ['bash','-c','curl -uadmin:password -X DELETE http://localhost:8088/artifactory/api/build/stm-test?deleteAll=1'].execute()
+        ArtifactoryRequest delete = new ArtifactoryRequestImpl().apiUrl("api/build/stm-test")
+          .setQueryParams(deleteAll: 1)
+          .method(ArtifactoryRequest.Method.DELETE)
+        artifactory.restCall(delete)
     }
 }
