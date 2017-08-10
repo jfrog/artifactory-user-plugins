37c2ae98d2111f93b652ad35f8ee008e999fd569
Elio Marcolino <eliom@jfrog.com>
2017-08-09 17:19:30 -0700

    Repo layout inforcement and indexing

--- cran/test/CranTest.groovy
+++ cran/test/CranTest.groovy
@@ -14,21 +14,36 @@ class CranTest extends Specification {
     def 'simple my plugin test'() {
         setup:
         def repo = createLocalCranRepo(repoKey)
-        def package1 = 'origin/cats_1.0.1.tgz'
-        def package2 = 'origin/cats_1.0.0.tgz'
-        def targetPath = 'destination/cats_1.0.1.tgz'
-        repo.upload(package1, new File('./src/test/groovy/CranTest/cats_1.0.1.tgz')).doUpload()
-        repo.upload(package2, new File('./src/test/groovy/CranTest/cats_1.0.0.tgz')).doUpload()
+//        def sourcePackage = 'fortunes_1.5-4.tar.gz'
+//        def macBinPackage = 'cats_1.0.1.tgz'
+//        def winBinPackage = 'Disake_1.5.zip'
+//        def package2 = 'cats_1.0.0.tgz'
+//        def package3 = 'AtmRay_1.31.tgz'
+//        def targetPath = 'destination/cats_1.0.1.tgz'
+//        repo.upload(sourcePackage, new File("./src/test/groovy/CranTest/$sourcePackage")).doUpload()
+//        repo.upload(macBinPackage, new File("./src/test/groovy/CranTest/$macBinPackage")).doUpload()
+//        repo.upload(winBinPackage, new File("./src/test/groovy/CranTest/$winBinPackage")).doUpload()
+//        repo.upload(package2, new File('./src/test/groovy/CranTest/cats_1.0.0.tgz')).doUpload()
+//        repo.upload(package3, new File('./src/test/groovy/CranTest/AtmRay_1.31.tgz')).doUpload()
 
         when:
+
+        def testDir = new File('./src/test/groovy/CranTest')
+        for (file in testDir.listFiles()) {
+            if (file.name.endsWith('.zip') || file.name.endsWith('.tar.gz') || file.name.endsWith('.tgz')) {
+                repo.upload(file.name, file).doUpload()
+            }
+        }
+
+
 //        moveArtifact("$repoKey/$originPath", "$repoKey/$targetPath")
-        sleep(1000)
+//        sleep(1000)
 //        repo.upload(targetPath, new ByteArrayInputStream('test'.bytes)).doUpload()
 
         // def json = new JsonSlurper().parseText(artifactory.plugins().execute('myPlugin').aSync())
 
 //        for (i in 1..2) {
-        artifactory.plugins().execute('cranIndex').withParameter('repoPath', 'repo/path/to/folder').aSync()
+//        artifactory.plugins().execute('cranIndex').withParameter('repoPath', 'repo/path/to/folder').aSync()
 //            sleep(1000)
 //        }
 //        sleep(1000)
