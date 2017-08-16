4fdc1293e5ba6aaed5ffaa052b148100bed137ac
Alix LOURME <alix.lourme@gmail.com>
2017-08-16 09:51:30 +0200

    Maven Snapshot Cleanup When Release feature

---
+++ cleanup/mavenSnapshotCleanupWhenRelease/MavenSnapshotCleanupWhenReleaseTest.groovy
@@ -0,0 +1,214 @@
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+import org.jfrog.artifactory.client.Artifactory
+import org.jfrog.artifactory.client.model.impl.SnapshotVersionBehaviorImpl
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+
+import groovyx.net.http.HttpResponseException
+import spock.lang.Specification
+
+class MavenSnapshotCleanupWhenReleaseTest extends Specification {
+
+    def TEST_FOLDER = 'org/jfrog/test'
+    def TEST_NAME = 'test-snapshot-cleanup-when-release'
+    def TEST_REPO_RELEASES = 'maven-local-releases'
+    def TEST_REPO_SNAPSHOTS = 'maven-local-snaphots'
+
+    private void createClientAndMavenRepo(Artifactory artifactory, String repoName, boolean handleReleases, boolean handleSnapshots){
+        createClientAndMavenRepo(artifactory, repoName, handleReleases, handleSnapshots, false)
+    }
+
+    private void createClientAndMavenRepo(Artifactory artifactory, String repoName, boolean handleReleases, boolean handleSnapshots, boolean suppressPomConsistency){
+        def builder = artifactory.repositories().builders()
+        def repoSettings = new MavenRepositorySettingsImpl()
+        repoSettings.handleReleases = handleReleases
+        repoSettings.handleSnapshots = handleSnapshots
+        repoSettings.suppressPomConsistencyChecks = suppressPomConsistency
+        if (handleSnapshots){
+            repoSettings.snapshotVersionBehavior = SnapshotVersionBehaviorImpl.unique
+        }
+
+        def repository = builder.localRepositoryBuilder().key(repoName)
+            .repositorySettings(repoSettings).build()
+
+        artifactory.repositories().create(0, repository)
+    }
+
+
+    def 'Maven Snapshot Cleanup When Release promotion move same repo test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        createClientAndMavenRepo(artifactory, 'maven-local', true, true, true)
+
+        artifactory.plugins().execute('mavenSnapshotCleanupWhenReleaseConfig').
+            withParameter('action', 'reset').withParameter('repositories', '[["maven-local","maven-local"]]').sync()
+
+        when:
+        // Double upload to generate -2 snapshot
+        uploadMavenArtifact(artifactory, 'maven-local', '1.0.0-SNAPSHOT')
+        def snapName = uploadMavenArtifact(artifactory, 'maven-local', '1.0.0-SNAPSHOT')
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName + '.jar').move('maven-local', TEST_FOLDER + '/' + TEST_NAME + '/1.0.0/'+TEST_NAME + '-1.0.0.jar')
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName + '.pom').move('maven-local', TEST_FOLDER + '/' + TEST_NAME + '/1.0.0/'+TEST_NAME + '-1.0.0.pom')
+
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        def metaData = artifactory.repository('maven-local').download(TEST_FOLDER + '/' + TEST_NAME + '/maven-metadata.xml').doDownload().text
+        assert !metaData.contains('1.0.0-SNAPSHOT')
+        assert metaData.contains('1.0.0')
+
+        then:
+        notThrown(HttpResponseException)
+
+        cleanup:
+        artifactory.repository('maven-local').delete()
+    }
+
+    def 'Maven Snapshot Cleanup When Release promotion move two repo test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        createClientAndMavenRepo(artifactory, TEST_REPO_RELEASES, true, false, true)
+        createClientAndMavenRepo(artifactory, TEST_REPO_SNAPSHOTS, false, true)
+        artifactory.plugins().execute('mavenSnapshotCleanupWhenReleaseConfig').
+            withParameter('action', 'reset').withParameter('repositories', '[["'+TEST_REPO_RELEASES+'","'+TEST_REPO_SNAPSHOTS+'"]]').sync()
+
+        when:
+        def snapName = uploadMavenArtifact(artifactory, TEST_REPO_SNAPSHOTS, '1.0.0-SNAPSHOT')
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName + '.jar').move(TEST_REPO_RELEASES, TEST_FOLDER + '/' + TEST_NAME + '/1.0.0/'+TEST_NAME + '-1.0.0.jar')
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName + '.pom').move(TEST_REPO_RELEASES, TEST_FOLDER + '/' + TEST_NAME + '/1.0.0/'+TEST_NAME + '-1.0.0.pom')
+
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        // Sometime metatData creation takes some time
+        sleep(3000)
+        def metaDataRelease = artifactory.repository(TEST_REPO_RELEASES).download(TEST_FOLDER + '/' + TEST_NAME + '/maven-metadata.xml').doDownload().text
+        assert metaDataRelease.contains('1.0.0')
+
+        then:
+        notThrown(HttpResponseException)
+
+        when:
+        artifactory.repository(TEST_REPO_SNAPSHOTS).download(TEST_FOLDER + '/' + TEST_NAME + '/maven-metadata.xml').doDownload().text
+
+        then:
+        thrown(HttpResponseException)
+
+        cleanup:
+        artifactory.repository(TEST_REPO_RELEASES).delete()
+        artifactory.repository(TEST_REPO_SNAPSHOTS).delete()
+    }
+
+    def 'Maven Snapshot Cleanup When Release disable test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        createClientAndMavenRepo(artifactory, TEST_REPO_RELEASES, true, false)
+        createClientAndMavenRepo(artifactory, TEST_REPO_SNAPSHOTS, false, true)
+        artifactory.plugins().execute('mavenSnapshotCleanupWhenReleaseConfig').withParameter('action', 'reset').sync()
+
+        when:
+        def snapName = uploadMavenArtifact(artifactory, TEST_REPO_SNAPSHOTS, '1.1.0-SNAPSHOT')
+        uploadMavenArtifact(artifactory, TEST_REPO_RELEASES, '1.1.0')
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.1.0-SNAPSHOT/' + snapName + '.pom').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        cleanup:
+        artifactory.repository(TEST_REPO_RELEASES).delete()
+        artifactory.repository(TEST_REPO_SNAPSHOTS).delete()
+    }
+
+    def 'Maven Snapshot Cleanup When Release same repo test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        createClientAndMavenRepo(artifactory, 'maven-local', true, true)
+
+        artifactory.plugins().execute('mavenSnapshotCleanupWhenReleaseConfig').
+            withParameter('action', 'reset').withParameter('repositories', '[["maven-local","maven-local"]]').sync()
+
+        def snapName = uploadMavenArtifact(artifactory, 'maven-local', '1.0.0-SNAPSHOT')
+        uploadMavenArtifact(artifactory, 'maven-local', '1.1.0-SNAPSHOT')
+
+        when:
+        uploadMavenArtifact(artifactory, 'maven-local', '1.1.0')
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.1.0-SNAPSHOT').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT').info()
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName  + '.pom').info()
+        artifactory.repository('maven-local').file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName  + '.jar').info()
+        def metaData = artifactory.repository('maven-local').download(TEST_FOLDER + '/' + TEST_NAME + '/maven-metadata.xml').doDownload().text
+        assert metaData.contains('1.0.0-SNAPSHOT')
+        assert !metaData.contains('1.1.0-SNAPSHOT')
+
+        then:
+        notThrown(HttpResponseException)
+
+        cleanup:
+        artifactory.repository('maven-local').delete()
+    }
+
+    def 'Maven Snapshot Cleanup When Release classic test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        createClientAndMavenRepo(artifactory, TEST_REPO_RELEASES, true, false)
+        createClientAndMavenRepo(artifactory, TEST_REPO_SNAPSHOTS, false, true)
+        artifactory.plugins().execute('mavenSnapshotCleanupWhenReleaseConfig').
+            withParameter('action', 'reset').withParameter('repositories', '[["'+TEST_REPO_RELEASES+'","'+TEST_REPO_SNAPSHOTS+'"]]').sync()
+
+        def snapName = uploadMavenArtifact(artifactory, TEST_REPO_SNAPSHOTS, '1.0.0-SNAPSHOT')
+        uploadMavenArtifact(artifactory, TEST_REPO_SNAPSHOTS, '1.1.0-SNAPSHOT')
+
+        when:
+        uploadMavenArtifact(artifactory, TEST_REPO_RELEASES, '1.1.0')
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.1.0-SNAPSHOT').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT').info()
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName + '.pom').info()
+        artifactory.repository(TEST_REPO_SNAPSHOTS).file(TEST_FOLDER + '/' + TEST_NAME + '/1.0.0-SNAPSHOT/' + snapName + '.jar').info()
+        def metaData = artifactory.repository(TEST_REPO_SNAPSHOTS).download(TEST_FOLDER + '/' + TEST_NAME + '/maven-metadata.xml').doDownload().text
+        assert metaData.contains('1.0.0-SNAPSHOT')
+        assert !metaData.contains('1.1.0-SNAPSHOT')
+
+        then:
+        notThrown(HttpResponseException)
+
+        cleanup:
+        artifactory.repository(TEST_REPO_RELEASES).delete()
+        artifactory.repository(TEST_REPO_SNAPSHOTS).delete()
+    }
+
+
+    // Return the file name without extention (could be modified when SNAPSHOT version due to Maven timestamp snapshot behavior)
+    private def uploadMavenArtifact(Artifactory artifactory, String repo, String version){
+        def pomContent = '<?xml version="1.0" encoding="UTF-8"?><project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd"><modelVersion>4.0.0</modelVersion><groupId>' + TEST_FOLDER.replace('/', '.') + '</groupId><artifactId>' + TEST_NAME + '</artifactId><version>'+version+'</version><packaging>jar</packaging></project>'
+        def pom = new ByteArrayInputStream(pomContent.bytes)
+        def jar = new ByteArrayInputStream('fake jar'.bytes)
+
+        def artifactPathPrefix = TEST_FOLDER + '/' + TEST_NAME + '/' + version + '/' + TEST_NAME + '-' + version
+
+        artifactory.repository(repo).upload(artifactPathPrefix + '.jar', jar).doUpload()
+        org.jfrog.artifactory.client.model.File file = artifactory.repository(repo).upload(artifactPathPrefix + '.pom', pom).doUpload()
+
+        return file.getName() - '.pom'
+    }
+}
---
+++ cleanup/mavenSnapshotCleanupWhenRelease/mavenSnapshotCleanupWhenRelease.groovy
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2014 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.artifactory.fs.ItemInfo
+import org.artifactory.repo.RepoPath
+import org.artifactory.repo.RepoPathFactory
+import org.jfrog.client.util.PathUtils
+
+import groovy.transform.Field
+
+@Field final String PROPERTIES_FILE_PATH = "plugins/${this.class.name}.properties"
+
+def config = new ConfigSlurper().parse(new File(ctx.artifactoryHome.haAwareEtcDir, PROPERTIES_FILE_PATH).toURI().toURL())
+def repositoriesMSCWR = [:]
+
+log.info "Check 'Maven Snapshot Cleanup When Release' plugin repositories list: $config.repositories"
+
+config.repositories.each{ repositorySettings ->
+    addRepositoriesSettings(repositoriesMSCWR, repositorySettings)
+}
+
+// curl command example for managing configuration of this plugin (prior Artifactory 5.x, use pipe '|' and not semi-colons ';' for parameters separation).
+//
+// curl -i -uadmin:password -X POST "http://localhost:8081/artifactory/api/plugins/execute/mavenSnapshotCleanupWhenReleaseConfig?params=action=reset;repositories=%5B%5B%22maven-local-lib-releases%22%5C%2C%22maven-local-lib-snapshots%22%5D%5C%2C%5B%22maven-local-plugin-releases%22%5C%2C%22maven-local-plugin-snapshots%22%5D%5D"
+//
+executions {
+    mavenSnapshotCleanupWhenReleaseConfig() { params ->
+        log.info "Update configuration with parameters: " + params
+        def action = params['action'] ? params['action'][0] as String : "add"
+        def repositoriesString = params['repositories'] ? params['repositories'][0] as String : "[]"
+        def configRepositories = new ConfigSlurper().parse('repositories=' + repositoriesString)
+
+        if (action == 'reset'){
+            log.info "Reseting configuration before adds"
+            repositoriesMSCWR.clear()
+        }
+
+        configRepositories.repositories.each{repositorySettings -> addRepositoriesSettings(repositoriesMSCWR, repositorySettings) }
+    }
+}
+
+storage {
+
+    // Standard use case, Maven release created in repository
+    afterCreate { ItemInfo item ->
+        removeMavenSnapshotIfRelease(item.getRepoPath(), repositoriesMSCWR)
+    }
+
+    // Promotion usage, Maven release moved from snapshot (but POM is not updated, should be done by another job => experimental)
+    afterMove { ItemInfo item, RepoPath targetRepoPath, properties ->
+        removeMavenSnapshotIfRelease(targetRepoPath, repositoriesMSCWR)
+    }
+
+    // Not used in this case, the pom file should be renamed with SNAPSHOT or timestamp => afterMove use case
+    // afterCopy {}
+
+}
+
+private void addRepositoriesSettings(def repositoriesMSCWR, def repositorySettings){
+    def release = repositorySettings[ 0 ] ? repositorySettings[ 0 ] as String : ["undefined"]
+    def snapshot = repositorySettings[ 1 ] ? repositorySettings[ 1 ] as String : ["undefined"]
+
+    def releaseConfig = repositories.getRepositoryConfiguration(release)
+    def snapshotConfig = repositories.getRepositoryConfiguration(snapshot)
+
+    if (releaseConfig && 'maven' == releaseConfig.getPackageType() && releaseConfig.isHandleReleases() && snapshotConfig && 'maven' == snapshotConfig.getPackageType() && snapshotConfig.isHandleSnapshots()){
+        log.debug "Add repositories couple $release/$snapshot"
+        repositoriesMSCWR[ release ] = snapshot
+    }else{
+        log.error "Skip repositories couple $release/$snapshot (does not exist or incorrect handle policy / package type)"
+    }
+}
+
+private void removeMavenSnapshotIfRelease(RepoPath repoPath, def repositoriesMSCWR){
+    if (repositoriesMSCWR [ repoPath.getRepoKey() ] &&  ! repoPath.isFolder() && repoPath.getPath().endsWith('.pom')){
+        def snapshotRepoPath = RepoPathFactory.create(repositoriesMSCWR [ repoPath.getRepoKey() ], PathUtils.getParent(repoPath.getPath()) + '-SNAPSHOT/')
+        if (repositories.exists(snapshotRepoPath)){
+            if (log.isInfoEnabled()){
+                log.info "Snapshot deletion due to release " + snapshotRepoPath
+            }
+            repositories.delete(snapshotRepoPath)
+        }
+    }
+}
