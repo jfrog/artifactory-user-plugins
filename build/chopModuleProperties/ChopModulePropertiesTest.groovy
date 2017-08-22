3e66bbd8c977786621b2994bcd859b106d3f7315
Sara Ahbabou <saraa@jfrog.com>
2017-08-22 13:53:23 -0700

    wrote tests

--- build/chopModuleProperties/ChopModulePropertiesTest.groovy
+++ build/chopModuleProperties/ChopModulePropertiesTest.groovy
@@ -1,19 +1,65 @@
 import spock.lang.Specification
 
 import static org.jfrog.artifactory.client.ArtifactoryClient.create
+import org.apache.http.client.HttpResponseException
+import org.jfrog.artifactory.client.ArtifactoryRequest
+import org.jfrog.artifactory.client.impl.ArtifactoryRequestImpl
+
+import groovy.json.JsonSlurper
 
 class ChopModulePropertiesTest extends Specification {
-    def 'test name'() {
-        setup:
-        def baseurl = 'http://localhost:8088/artifactory'
-        def artifactory = create(baseurl, 'admin', 'password')
+ def 'ChopModulePropertiesTest name' () {
 
-        when:
-        null
+  setup: def baseurl = 'http://localhost:8088/artifactory'
+  def artifactory = create(baseurl, 'admin', 'password')
 
-        then:
-        false
+  def file = new File("./src/test/groovy/build.json")
+  def request = new JsonSlurper().parse(file)
 
-        //cleanup:
-    }
+  ArtifactoryRequest uploadBuild = new ArtifactoryRequestImpl().apiUrl("api/build")
+  .method(ArtifactoryRequest.Method.PUT)
+  .requestType(ArtifactoryRequest.ContentType.JSON)
+  .requestBody(new JsonSlurper().parse(file))
+
+  artifactory.restCall(uploadBuild)
+
+
+  when: ArtifactoryRequest getBuildInfo = new ArtifactoryRequestImpl().apiUrl("api/build/test-build/1")
+   .method(ArtifactoryRequest.Method.GET)
+   .responseType(ArtifactoryRequest.ContentType.JSON)
+
+   def response = artifactory.restCall(getBuildInfo)
+
+
+   then:
+
+   boolean isGood = true
+
+   response.buildInfo.modules.each {
+   m ->
+    print "m.properties: ${m.properties}"
+   Map < String, String > changed = [: ]
+   m.properties.each {
+    String k, String v ->
+     if (v.length() > 899) {
+      isGood = false
+
+     }
+   }
+  }
+
+   isGood
+
+
+   cleanup:
+
+   ArtifactoryRequest delete = new ArtifactoryRequestImpl().apiUrl("api/build/test-build")
+   .setQueryParams(deleteAll: 1)
+   .method(ArtifactoryRequest.Method.DELETE)
+  artifactory.restCall(delete)
+
+ }
 }
+
+
+
