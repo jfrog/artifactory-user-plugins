822adfe6ebeac408c84520ff604758ad0da0bd32
Mari Yamaguchi <mariy@ip-10-6-18-143.eu-west-1.compute.internal>
2017-08-29 16:56:40 -0700

    test changes

--- build/buildPropertySetter/test/BuildPropertySetterTest.groovy
+++ build/buildPropertySetter/test/BuildPropertySetterTest.groovy
@@ -20,11 +20,12 @@ class BuildPropertySetterTest extends Specification {
             artifactory.repositories().create(0, local)
 
 
-
         def pom = new File('./src/test/groovy/BuildPropertySetterTest/pom.xml')
         def pom2 = new File('./src/test/groovy/BuildPropertySetterTest/pom2.xml')
         def path = "group/artifact/1.0-SNAPSHOT/artifact-1.0-20170828.232033-1.pom"
         def path2 = "group/artifact/2.0-SNAPSHOT/artifact-1.0-20170828.232043-2.pom"
+        def file = new File('./src/test/groovy/BuildPropertySetterTest/build.json')
+        def file2 = new File('./src/test/groovy/BuildPropertySetterTest/build2.json')
         
 
         when:
@@ -34,21 +35,27 @@ class BuildPropertySetterTest extends Specification {
             .withProperty("build.number","20")
             .doUpload(); 
 
-        artifactory.repository("libs-snapshot-local")
-            .upload(path2,pom2)
-            .withProperty("build.name","unit-test")
-            .withProperty("build.number","21")
-            .doUpload(); 
-
-
-        def file = new File('./src/test/groovy/BuildPropertySetterTest/build.json')
-        def file2 = new File('./src/test/groovy/BuildPropertySetterTest/build2.json')
-
         ArtifactoryRequest uploadBuild = new ArtifactoryRequestImpl().apiUrl("api/build")
           .method(ArtifactoryRequest.Method.PUT)
           .requestType(ArtifactoryRequest.ContentType.JSON)
           .requestBody(new JsonSlurper().parse(file))
         artifactory.restCall(uploadBuild)
+
+        def check = artifactory.searches()
+            .repositories("libs-snapshot-local")  
+            .itemsByProperty()
+            .property("latest","true")
+            .property("build.name","unit-test")
+            .property("build.number","20")
+            .doSearch(); 
+        check.size() == 1
+
+
+        artifactory.repository("libs-snapshot-local")
+            .upload(path2,pom2)
+            .withProperty("build.name","unit-test")
+            .withProperty("build.number","21")
+            .doUpload();     
 
         ArtifactoryRequest uploadBuild2 = new ArtifactoryRequestImpl().apiUrl("api/build")
           .method(ArtifactoryRequest.Method.PUT)
@@ -58,13 +65,16 @@ class BuildPropertySetterTest extends Specification {
 
      
         then:
-        def check = artifactory.searches()
+        def check2 = artifactory.searches()
             .repositories("libs-snapshot-local")  
             .itemsByProperty()
             .property("latest","true")
             .property("build.name","unit-test")
             .property("build.number","21")
             .doSearch(); 
+        check2.size() == 1
+
+        def check3 = check.size() == 0
 
         cleanup:
         artifactory.repository("libs-snapshot-local").delete()
