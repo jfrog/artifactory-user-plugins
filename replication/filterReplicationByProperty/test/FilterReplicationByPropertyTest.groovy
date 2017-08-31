3b4b00376d575245b47f2ba4ec03d8d02389ba3b
jainish shah <jainishshah@yahoo.com>
2017-08-31 14:26:05 -0700

    Added tests for replication plugins

---
+++ replication/filterReplicationByProperty/test/FilterReplicationByPropertyTest.groovy
@@ -0,0 +1,63 @@
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+import spock.lang.Specification
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class FilterReplicationByPropertyTest extends Specification {
+
+   def 'filter replication by property test'() {
+        setup:
+        def baseurl1 = 'http://localhost:8088/artifactory'
+        def baseurl2 = 'http://localhost:8081/artifactory'
+        def artifactory1 = create(baseurl1, 'admin', 'password')
+        def artifactory2 = create(baseurl2, 'admin', 'password')
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        def jarfile = new File('./src/test/groovy/FilterReplicationByPropertyTest/lib-aopalliance-1.0.jar')
+
+        def builder1 = artifactory1.repositories().builders()
+        def local = builder1.localRepositoryBuilder().key('libs-release-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory1.repositories().create(0, local)
+
+        def builder2 = artifactory2.repositories().builders()
+        def copy = builder2.localRepositoryBuilder().key('libs-release-local')
+                .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory2.repositories().create(0, copy)
+
+        def conn = new URL("${baseurl1}/api/replications/libs-release-local").openConnection()
+        conn.requestMethod = 'PUT'
+        conn.doOutput = true
+        conn.setRequestProperty('Authorization', auth)
+        conn.setRequestProperty('Content-Type', 'application/json')
+
+        def textFile = "{\"url\" : \"${baseurl2}/libs-release-local\","
+        textFile += '"socketTimeoutMillis" : 15000,'
+        textFile += '"username" : "admin",'
+        textFile += '"password" : "password",'
+        textFile += '"enableEventReplication" : true,'
+        textFile += '"enabled" : true,'
+        textFile += '"cronExp" : "0 0 12 * * ?",'
+        textFile += '"syncDeletes" : true,'
+        textFile += '"syncProperties" : true,'
+        textFile += '"syncStatistics" : false,'
+        textFile += '"repoKey" : "libs-release-local"}'
+        conn.outputStream.write(textFile.bytes)
+        assert conn.responseCode == 201
+        conn.disconnect()
+
+       when:
+        artifactory1.repository("libs-release-local")
+        .upload("lib-aopalliance-1.0.jar", jarfile)
+        .withProperty("foo", "true")
+        .doUpload()
+
+        sleep(1000000)
+
+        then:
+        artifactory2.repository("libs-release-local").file("lib-aopalliance-1.0.jar").info()
+
+        cleanup:
+        artifactory1.repository("libs-release-local").delete()
+        artifactory2.repository("libs-release-local").delete()
+    }
+}
---
+++ replication/yumReplicationFilter/test/YumReplicationFilterTest.groovy
@@ -0,0 +1,62 @@
+import org.jfrog.artifactory.client.model.repository.settings.impl.YumRepositorySettingsImpl
+import spock.lang.Specification
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class YumReplicationFilterTest extends Specification {
+
+   def 'yum replication filter'() {
+        setup:
+        def baseurl1 = 'http://localhost:8088/artifactory'
+        def baseurl2 = 'http://localhost:8081/artifactory'
+        def artifactory1 = create(baseurl1, 'admin', 'password')
+        def artifactory2 = create(baseurl2, 'admin', 'password')
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        def rpmfile = new File('./src/test/groovy/YumReplicationFilterTest/wget-1.19.1-3.fc27.aarch64.rpm')
+
+        def builder1 = artifactory1.repositories().builders()
+        def local = builder1.localRepositoryBuilder().key('yum-local')
+        .repositorySettings(new YumRepositorySettingsImpl()).build()
+        artifactory1.repositories().create(0, local)
+
+        def builder2 = artifactory2.repositories().builders()
+        def copy = builder2.localRepositoryBuilder().key('yum-local')
+        .repositorySettings(new YumRepositorySettingsImpl()).build()
+        artifactory2.repositories().create(0, copy)
+        
+        def conn = new URL("${baseurl1}/api/replications/yum-local").openConnection()
+        conn.requestMethod = 'PUT'
+        conn.doOutput = true
+        conn.setRequestProperty('Authorization', auth)
+        conn.setRequestProperty('Content-Type', 'application/json')
+
+        def textFile = "{\"url\" : \"${baseurl2}/yum-local\","
+        textFile += '"socketTimeoutMillis" : 15000,'
+        textFile += '"username" : "admin",'
+        textFile += '"password" : "password",'
+        textFile += '"enableEventReplication" : true,'
+        textFile += '"enabled" : true,'
+        textFile += '"cronExp" : "0 0 12 * * ?",'
+        textFile += '"syncDeletes" : true,'
+        textFile += '"syncProperties" : true,'
+        textFile += '"syncStatistics" : false,'
+        textFile += '"repoKey" : "yum-local"}'
+        conn.outputStream.write(textFile.bytes)
+        assert conn.responseCode == 201
+        conn.disconnect()
+
+        when:
+        artifactory1.repository("yum-local")
+        .upload("wget-1.19.1-3.fc27.aarch64.rpm", rpmfile)
+        .doUpload()
+
+        sleep(1000000)
+
+        then:
+        artifactory2.repository("yum-local").file("wget-1.19.1-3.fc27.aarch64.rpm").info()
+
+        cleanup:
+        artifactory1.repository("yum-local").delete()
+        artifactory2.repository("yum-local").delete()
+    }
+}
