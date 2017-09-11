ff61e1b2c95f6ebb682663ba84a3696a7803360e
travisf <travisf@jfrog.com>
2017-09-11 14:45:47 -0700

    Added test for DBSyncNotification

--- DB/DBSyncNotification/DBSyncNotificationTest.groovy
+++ DB/DBSyncNotification/DBSyncNotificationTest.groovy
@@ -1,19 +1,48 @@
+import groovy.json.JsonSlurper
 import spock.lang.Specification
 
-import static org.jfrog.artifactory.client.ArtifactoryClient.create
-
 class DBSyncNotificationTest extends Specification {
-    def 'test name'() {
-        setup:
-        def baseurl = 'http://localhost:8088/artifactory'
-        def artifactory = create(baseurl, 'admin', 'password')
-
+    def 'db sync notif test'() {
         when:
-        null
+        def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
+        def logs = 'http://localhost:8088/artifactory/api/systemlogs/logData?id=artifactory.log'
+        def sync = 'http://localhost:8081/artifactory/api/plugins/execute/syncNotification'
+        def lastline = null, conn = null, lastlinum = -1, startlinum = -1
+        // the test won't pass unless we wait for the HA nodes to connect
+        while (true) {
+            conn = new URL(logs).openConnection()
+            conn.setRequestProperty('Authorization', auth)
+            assert conn.responseCode == 200
+            def oldlines = new JsonSlurper().parse(conn.inputStream).logContent.readLines()
+            conn.disconnect()
+            lastline = oldlines.findAll { it ==~ /.*\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.*/ }[-1]
+            if (lastlinum > startlinum) break
+            lastlinum = oldlines.findIndexOf { it == lastline }
+            startlinum = oldlines.findIndexOf { it.contains('Artifactory successfully started') }
+            System.sleep(5000)
+        }
+        conn = new URL(sync).openConnection()
+        conn.requestMethod = 'POST'
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        conn.disconnect()
+        System.sleep(1000)
+        conn = new URL(logs).openConnection()
+        conn.setRequestProperty('Authorization', auth)
+        assert conn.responseCode == 200
+        def lines = new JsonSlurper().parse(conn.inputStream).logContent.readLines()
+        conn.disconnect()
+        def idx = lines.findIndexOf { it == lastline }
+        def newlines = lines[idx + 1 .. -1]
 
         then:
-        false
-
-        //cleanup:
+        idx > 0
+        newlines.size() > 0
+        newlines.any {
+            it.contains('Artifactory application context set to NOT READY by reload') || it.contains('Reloading configuration...')
+        }
+        newlines.any {
+            it.contains('Artifactory application context set to READY by reload') || it.contains('Configuration reloaded.')
+        }
     }
 }
--- DB/DBSyncNotification/README.md
+++ DB/DBSyncNotification/README.md
@@ -1,1 +1,20 @@
-*This plugin is currently being tested for Artifactory 5.x releases.*
+Artifactory DB Sync Notification User Plugin
+============================================
+
+This plugin allows you to force a database sync in an HA cluster.
+
+For efficiency reasons, Artifactory caches some database resources locally
+(configuration files, permissions information, etc). In an HA cluster, each node
+has its own cache, so to stay up to date with each other, the nodes send
+database sync messages whenever a cached resource changes. This plugin allows
+you to send these messages manually, whenever you'd like.
+
+Usage
+-----
+
+This plugin exposes a REST endpoint that, when called, sends database sync
+messages to all nodes in the HA cluster. For example:
+
+``` shell
+curl -v -uadmin:password -XPOST http://localhost:8081/artifactory/api/plugins/execute/syncNotification
+```
