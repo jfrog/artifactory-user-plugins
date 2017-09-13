9f80b2b641a0356a77cbb6b14e9dfed3a2d2b393
travisf <travisf@jfrog.com>
2017-09-13 10:24:00 -0700

    Fixed metadata/addPypiNormalizedName
    Fixed this plugin, and improved its test and its readme.

--- metadata/addPypiNormalizedName/README.md
+++ metadata/addPypiNormalizedName/README.md
@@ -1,38 +1,52 @@
-This Artifactory User Plugin is for Pypi packages in version 1.2 that aren't correctly generating the pypi.normalized.name property, despite having the correct metadata file(s).
+Artifactory Add PyPi Normalized Name User Plugin
+================================================
 
-It will only work in Pypi Local Repositories, with artifacts that already have a pypi.name property, but not a pypi.normalized.name.
+This Artifactory User Plugin is for Pypi packages in version 1.2 that aren't
+correctly generating the `pypi.normalized.name` property, despite having the
+correct metadata file(s).
 
-This plugin has been tested in Artifactory Pro 5.2.x and 4.9.x using the supplied testing file.
+It will only work in Pypi local repositories, with artifacts that already have a
+`pypi.name` property, but not a `pypi.normalized.name`.
 
-Installation:
+Installation
+------------
 
-Place the PypiPropertyPlugin.groovy file into the $ARTIFACTORY_HOME/etc/plugins/ folder. You will need to restart Artifactory unless you have it set to scan for plugin changes. That can be done by setting
+Place the `PypiPropertyPlugin.groovy` file into the
+`$ARTIFACTORY_HOME/etc/plugins/` folder. You will need to restart Artifactory
+unless you have it set to scan for plugin changes. That can be done by setting
+the following in `$ARTIFACTORY_HOME/etc/artifactory.system.properties`:
 
+```
 artifactory.plugin.scripts.refreshIntervaleSecs= [value greater than 0]
+```
 
-in $ARTIFACTORY_HOME/etc/artifactory.system.properties.
+To enable logging, find the logback file located at
+`$ARTIFACTORY_HOME/etc/logback.xml` and add the following lines at the bottom:
 
-To enable logging, find the logback file located at $ARTIFACTORY_HOME/etc/logback.xml
-
-and add the following lines at the bottom:
-
+``` xml
 <logger name = "pypiPropertyPlugin">
   <level value = "info">
 </logger>
-You should be able to see the script logging start in Artifactory.log.
+```
 
-Usage:
+You should be able to see the script logging start in `artifactory.log`.
 
-By default, it runs every 30 seconds, this can be changed by editing the interval parameter interval on line 7.
+Usage
+-----
 
+By default, the plugin runs every 30 seconds. This can be changed by editing the
+`interval` parameter on line 7:
+
+``` groovy
 addPerms(interval: 30000, delay: 100) {
+```
 
-Add the Pypi repositories that you wish to have monitored by the script on line 10, by adding it to the provided array searchRepoNames.
+Add the Pypi repositories that you wish to have monitored by the script on line
+10, by adding it to the provided array `searchRepoNames`.
 
+``` groovy
 def searchRepoNames = []
+```
 
-Please note that if new repositories are added, the script must be reloaded before it can detect them for property addition.
-
-Testing:
-
-Testing was done using the Pypi package binparse, but can be substituted for any valid Pypi Package. Testing has been provided in the pypiPropertyPluginTest.groovy file.
+Please note that if new repositories are added, the script must be reloaded
+before it can detect them for property addition.
---
+++ metadata/addPypiNormalizedName/test/AddPypiNormalizedNameTest.groovy
@@ -0,0 +1,75 @@
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+import org.jfrog.artifactory.client.model.repository.settings.impl.PypiRepositorySettingsImpl
+import spock.lang.Specification
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class AddPypiNormalizedNameTest extends Specification {
+    def 'pypi normalized name test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        def stream = new File('./src/test/groovy/AddPypiNormalizedNameTest/binparse-1.2.tar.gz')
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('pypi-local')
+        .repositorySettings(new PypiRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+        artifactory.repository('pypi-local').upload('pypi-bug/binparse-test-1.2.tar.gz', stream).doUpload()
+
+        when:
+        artifactory.repository('pypi-local').file('pypi-bug/binparse-test-1.2.tar.gz').deleteProperty('pypi.name')
+        artifactory.repository('pypi-local').file('pypi-bug/binparse-test-1.2.tar.gz').deleteProperty('pypi.normalized.name')
+        artifactory.repository('pypi-local').file('pypi-bug/binparse-test-1.2.tar.gz').properties().addProperty('pypi.name', 'TEST_NAME').doSet()
+        sleep(5000)
+
+        then:
+        artifactory.repository('pypi-local').file('pypi-bug/binparse-test-1.2.tar.gz').getPropertyValues('pypi.normalized.name')[0] == 'test-name'
+
+        cleanup:
+        artifactory.repository("pypi-local").delete()
+    }
+
+    def 'non pypi dir test'(){
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('maven-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+        def package1 = new File('./src/test/groovy/AddPypiNormalizedNameTest/binparse-1.2.tar.gz')
+
+        when:
+        artifactory.repository('maven-local').upload('com/google/guava/1.2/binparse-test-1.2.tar.gz', package1).doUpload()
+        sleep(5000)
+
+        then:
+        artifactory.repository('maven-local').file('com/google/guava/1.2/binparse-test-1.2.tar.gz').getPropertyValues('pypi.normalized.name') == null
+
+        cleanup:
+        artifactory.repository('maven-local').delete()
+    }
+
+    def 'pypi dir no pypi.name test'(){
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('pypi-local2')
+        .repositorySettings(new PypiRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+        def package1 = new File('./src/test/groovy/AddPypiNormalizedNameTest/binparse-1.2.tar.gz')
+
+        when:
+        artifactory.repository('pypi-local2').upload('pypi-bug2/binparse-test-1.2.tar.gz', package1).doUpload()
+        artifactory.repository('pypi-local2').file('pypi-bug2/binparse-test-1.2.tar.gz').deleteProperty('pypi.name')
+        artifactory.repository('pypi-local2').file('pypi-bug2/binparse-test-1.2.tar.gz').deleteProperty('pypi.normalized.name')
+        sleep(5000)
+
+        then:
+        artifactory.repository('pypi-local2').file('pypi-bug2/binparse-test-1.2.tar.gz').getPropertyValues('pypi.normalized.name') == null
+
+        cleanup:
+        artifactory.repository('pypi-local2').delete()
+    }
+}
