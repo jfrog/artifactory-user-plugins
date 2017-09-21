6a73a4a413fb955066fd040896822fcbd8018ebf
travisf <travisf@jfrog.com>
2017-09-21 14:33:50 -0700

    Fixed snapCenter dependency issue

--- netApp/snapCenter/setup.groovy
+++ netApp/snapCenter/setup.groovy
@@ -3,5 +3,6 @@ artifactory 8088, {
     dependency 'org.codehaus.groovy.modules.http-builder:http-builder:0.7.2'
     dependency 'net.sf.json-lib:json-lib:2.4:jdk15'
     dependency 'xml-resolver:xml-resolver:1.2'
+    dependency 'net.sf.ezmorph:ezmorph:1.0.6'
     sed 'body.json', /localhost/, localhost
 }
