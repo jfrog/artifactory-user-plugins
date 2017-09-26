5e04205a7aaf0cf634787c1799133e944187629f
travisf <travisf@jfrog.com>
2017-09-26 10:07:00 -0700

    Fixed ivy2pom loading typo

--- descriptors/ivy2pom/setup.groovy
+++ descriptors/ivy2pom/setup.groovy
@@ -1,5 +1,5 @@
 artifactory 8088, {
-    plugin 'descriptors/pom2ivy'
+    plugin 'descriptors/ivy2pom'
     dependency 'org.jwaresoftware.antxtras:jw-antxtras:3.0.0'
     dependency 'org.jwaresoftware.antxtras:jw-log4ant:3.0.0'
     dependency 'org.apache.ant:ant:1.8.3'
