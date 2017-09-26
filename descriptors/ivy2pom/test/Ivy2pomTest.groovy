84c4e4d419cd2a20519e80339000cc2493acfb1c
travisf <travisf@jfrog.com>
2017-09-26 14:54:22 -0700

    Fixed ivy2pom and pom2ivy tests

---
+++ descriptors/ivy2pom/test/Ivy2pomTest.groovy
@@ -0,0 +1,57 @@
+import spock.lang.Specification
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+import org.jfrog.artifactory.client.model.repository.settings.impl.IvyRepositorySettingsImpl
+import org.jfrog.pluginsdevenv.Control
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class Ivy2pomTest extends Specification {
+    def 'simple ivy to pom plugin test'() {
+        setup:
+        // ant jars need to be in the root Artifactory lib directory
+        moveAntJars()
+
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+
+        def builder = artifactory.repositories().builders()
+        def ivy = builder.localRepositoryBuilder().key('ivy-local')
+        .repositorySettings(new IvyRepositorySettingsImpl())
+        .repoLayoutRef('ivy-default').build()
+        artifactory.repositories().create(0, ivy)
+
+        def pom = builder.localRepositoryBuilder().key('pom-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, pom)
+
+        def ivypath = 'myorg/mymodule/2.0/nulls/ivy-2.0.xml'
+        def pompath = 'myorg/mymodule/2.0/mymodule-2.0.pom'
+        def xml = "<ivy-module version='2.0'>"
+        xml += "<info organisation='myorg' module='mymodule' revision='2.0' />"
+        xml += "</ivy-module>"
+        artifactory.repository('ivy-local').upload(ivypath, new ByteArrayInputStream(xml.bytes)).doUpload()
+
+        when:
+        def pomfile = new XmlParser().parse(artifactory.repository('pom-local').download(pompath).doDownload())
+
+        then:
+        pomfile.groupId.text() == 'myorg'
+        pomfile.artifactId.text() == 'mymodule'
+        pomfile.version.text() == '2.0'
+
+        cleanup:
+        artifactory.repository('pom-local').delete()
+        artifactory.repository('ivy-local').delete()
+    }
+
+    private moveAntJars() {
+        def src = './src/test/groovy/Ivy2pomTest/'
+        def dst = '/var/opt/jfrog/artifactory/tomcat/webapps/artifactory/WEB-INF/lib/'
+        def jar1 = 'ant-1.8.3.jar', jar2 = 'ant-launcher-1.8.3.jar'
+        Control.setFileContent(8088, dst + jar1, new File(src + jar1))
+        Control.setFileContent(8088, dst + jar2, new File(src + jar2))
+        Control.stop(8088)
+        Control.resume(8088)
+        System.sleep(5000)
+    }
+}
---
+++ descriptors/pom2ivy/test/Pom2ivyTest.groovy
@@ -0,0 +1,60 @@
+import spock.lang.Specification
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+import org.jfrog.artifactory.client.model.repository.settings.impl.IvyRepositorySettingsImpl
+import org.jfrog.pluginsdevenv.Control
+
+import static org.jfrog.artifactory.client.ArtifactoryClient.create
+
+class Pom2ivyTest extends Specification {
+    def 'simple pom to ivy plugin test'() {
+        setup:
+        // ant jars need to be in the root Artifactory lib directory
+        moveAntJars()
+
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = create(baseurl, 'admin', 'password')
+
+        def builder = artifactory.repositories().builders()
+        def ivy = builder.localRepositoryBuilder().key('ivy-local')
+        .repositorySettings(new IvyRepositorySettingsImpl())
+        .repoLayoutRef('ivy-default').build()
+        artifactory.repositories().create(0, ivy)
+
+        def pom = builder.localRepositoryBuilder().key('pom-local')
+        .repositorySettings(new MavenRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, pom)
+
+        def ivypath = 'com.mycompany.app/my-app/1.0/nulls/ivy-1.0.xml'
+        def pompath = 'com/mycompany/app/my-app/1.0/my-app-1.0.pom'
+        def xml = "<project>"
+        xml += "<modelVersion>4.0.0</modelVersion>"
+        xml += "<groupId>com.mycompany.app</groupId>"
+        xml += "<artifactId>my-app</artifactId>"
+        xml += "<version>1.0</version>"
+        xml += "</project>"
+        artifactory.repository('pom-local').upload(pompath, new ByteArrayInputStream(xml.bytes)).doUpload()
+
+        when:
+        def ivyfile = new XmlParser().parse(artifactory.repository('ivy-local').download(ivypath).doDownload())
+
+        then:
+        ivyfile.info[0].@organisation == 'com.mycompany.app'
+        ivyfile.info[0].@module == 'my-app'
+        ivyfile.info[0].@revision == '1.0'
+
+        cleanup:
+        artifactory.repository('ivy-local').delete()
+        artifactory.repository('pom-local').delete()
+    }
+
+    private moveAntJars() {
+        def src = './src/test/groovy/Pom2ivyTest/'
+        def dst = '/var/opt/jfrog/artifactory/tomcat/webapps/artifactory/WEB-INF/lib/'
+        def jar1 = 'ant-1.8.3.jar', jar2 = 'ant-launcher-1.8.3.jar'
+        Control.setFileContent(8088, dst + jar1, new File(src + jar1))
+        Control.setFileContent(8088, dst + jar2, new File(src + jar2))
+        Control.stop(8088)
+        Control.resume(8088)
+        System.sleep(5000)
+    }
+}
