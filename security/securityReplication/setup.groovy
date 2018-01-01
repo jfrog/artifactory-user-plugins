10b659f7781edad037155f7d88af3a123b1cc9f6
Stanley Fong <stanleyf@jfrog.com>
2018-01-01 15:56:11 -0800

    Set safety false for CI automated tests.

--- security/securityReplication/setup.groovy
+++ security/securityReplication/setup.groovy
@@ -2,6 +2,7 @@ artifactory 8088, {
     plugin "security/securityReplication"
     sed 'securityReplication.json', /"filter": 1/, '"filter": 3'
     sed 'securityReplication.json', /"0 0 0\/1 \* \* \?"/, '"0 0 0 * * ? 2000"'
+    sed 'securityReplication.json', /"safety": true/, '"safety": false'
     // node 8081
     // node 8082
 }
@@ -10,6 +11,7 @@ artifactory 8090, {
     plugin "security/securityReplication"
     sed 'securityReplication.json', /"filter": 1/, '"filter": 3'
     sed 'securityReplication.json', /"0 0 0\/1 \* \* \?"/, '"0 0 0 * * ? 2000"'
+    sed 'securityReplication.json', /"safety": true/, '"safety": false'
     sed 'securityReplication.json', /whoami": "http:\/\/localhost:8088/, 'whoami": "http://localhost:8090'
     // node 8083
     // node 8084
@@ -19,6 +21,7 @@ artifactory 8091, {
     plugin "security/securityReplication"
     sed 'securityReplication.json', /"filter": 1/, '"filter": 3'
     sed 'securityReplication.json', /"0 0 0\/1 \* \* \?"/, '"0 0 0 * * ? 2000"'
+    sed 'securityReplication.json', /"safety": true/, '"safety": false'
     sed 'securityReplication.json', /whoami": "http:\/\/localhost:8088/, 'whoami": "http://localhost:8091'
 }
 
@@ -26,5 +29,6 @@ artifactory 8092, {
     plugin "security/securityReplication"
     sed 'securityReplication.json', /"filter": 1/, '"filter": 3'
     sed 'securityReplication.json', /"0 0 0\/1 \* \* \?"/, '"0 0 0 * * ? 2000"'
+    sed 'securityReplication.json', /"safety": true/, '"safety": false'
     sed 'securityReplication.json', /whoami": "http:\/\/localhost:8088/, 'whoami": "http://localhost:8092'
 }
