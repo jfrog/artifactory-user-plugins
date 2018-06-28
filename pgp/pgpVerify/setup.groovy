966be9405c74ae2b50c9f1b112d338bf2be392a4
Elio Marcolino <eliom@jfrog.com>
2018-06-28 14:42:04 -0700

    Changed pgpVerify test strategy

--- pgp/pgpVerify/setup.groovy
+++ pgp/pgpVerify/setup.groovy
@@ -1,7 +1,8 @@
 artifactory 8088, {
     plugin 'pgp/pgpVerify'
     sed 'pgpVerify.properties', /repos=.*/, "repos=[\'maven-local\']"
+    sed 'pgpVerify.groovy', /http:\/\/pgp.mit.edu:11371/, "http://localhost:8000"
     dependency 'org.codehaus.groovy.modules.http-builder:http-builder:0.7.2'
     dependency 'net.sf.json-lib:json-lib:2.4:jdk15'
-    dependency 'xml-resolver:xml-resolver:1.2'    
+    dependency 'xml-resolver:xml-resolver:1.2'
 }
--- pgp/pgpVerify/test/PgpVerifyTest.groovy
+++ pgp/pgpVerify/test/PgpVerifyTest.groovy
@@ -1,9 +1,15 @@
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
 import spock.lang.Specification
 import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
 import org.jfrog.artifactory.client.ArtifactoryClientBuilder
 import org.apache.http.client.HttpResponseException
-
+    
 class PgpVerifyTest extends Specification {
+
+    static final def mockServerPort = 8000
+
     def 'pgp verify test'() {
         setup:
         def baseurl = 'http://localhost:8088/artifactory'
@@ -24,6 +30,9 @@ class PgpVerifyTest extends Specification {
         artifactory.repository('maven-local').upload('bar/maven-metadata.xml', xmlfile).doUpload()
         artifactory.repository('maven-local').upload('bar/maven-metadata.xml.asc', badfile).doUpload()
 
+        def mockServer = new PGPPublicKeyMockServer(port: mockServerPort)
+        mockServer.start()
+
         when:
         artifactory.repository('maven-local').download('foo/maven-metadata.xml').doDownload()
 
@@ -37,6 +46,46 @@ class PgpVerifyTest extends Specification {
         thrown(HttpResponseException)
 
         cleanup:
+        mockServer.stop()
         artifactory.repository('maven-local').delete()
     }
+
+    /**
+     * PGP Public Key Mock Server
+     */
+    class PGPPublicKeyMockServer {
+
+        HttpServer server
+        int port = 8000
+
+        def start() {
+            InetSocketAddress address = new InetSocketAddress(port);
+            server = HttpServer.create(address, 0);
+
+            HttpHandler slackRequestValidatorHandler = new HttpHandler() {
+                @Override
+                void handle(HttpExchange exchange) throws IOException {
+                    println "Received a new request"
+                    def content = exchange.requestBody.text
+                    println "Message content: $content"
+                    def responseFile = new File('./src/test/groovy/PgpVerifyTest/public_key.asc')
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, responseFile.length())
+                    OutputStream os = exchange.getResponseBody()
+                    os.write(responseFile.bytes)
+                    os.close()
+                    exchange.close()
+                }
+            }
+
+            server.createContext("/", slackRequestValidatorHandler)
+            server.start()
+            println "Mock server started"
+        }
+
+        def stop() {
+            server?.stop(0)
+            println "Mock server stopped"
+        }
+    }
+
 }
