d647903b6d47d6fe49c5f4e249adfcbf7d9f208f
Quinten Krijger <qkrijger@gmail.com>
2019-03-05 16:40:39 +0100

    Alpine is now supported via a plugin

---
+++ repository/alpine/README.md
@@ -0,0 +1,15 @@
+# Artifactory <Name> User Plugin
+
+<Provide a brief description on what this plugins does and the scenarios when it can/should be used.>
+
+## Features
+
+<List and describe in more details the plugin features>
+
+## Installation
+
+<Describe how to get this plugin up and running>
+
+## Usage
+
+<Describe how to use the features delivered by this plugin>
---
+++ repository/alpine/alpine.groovy
@@ -0,0 +1,24 @@
+import org.artifactory.repo.RepoPath
+import org.artifactory.repo.RepoPathFactory
+
+jobs {
+	deleteApkIndexFiles(delay: 0, interval: 60000) {
+        alpineRepos = getAlpineRepos()
+        alpineCaches = alpineRepos.collect { it.getRepoKey() + '-cache' }
+        indexFiles = searches.artifactsByName('APKINDEX.tar.gz', *alpineCaches)
+        log.info("Found apk index files that will be deleted: {}", indexFiles)
+        indexFiles.each {
+            log.warn("deleting apk index files: {}", it)
+            repositories.delete(it)
+        }
+    }
+}
+
+/**
+ * Returns a list of local alpine repositories, determined by the existence of the 'alpine' property.
+ */
+List<RepoPath> getAlpineRepos() {
+	repositories.getRemoteRepositories()
+		.collect { RepoPathFactory.create(it) }
+		.findAll { repositories.hasProperty(it, 'alpine') }
+}
