731dc4ae6ebff5bcf9873a95c91464190d8a9451
Quinten Krijger <qkrijger@gmail.com>
2019-03-14 16:16:53 +0100

    Document the alpine plugin job

--- repository/alpine/alpine.groovy
+++ repository/alpine/alpine.groovy
@@ -1,6 +1,13 @@
 import org.artifactory.repo.RepoPath
 import org.artifactory.repo.RepoPathFactory
 
+/**
+ * A CRON job to remove all apk index files from cache repositories marked 'alpine'.
+ * Adjust the interval (in ms) at will. Note that when the interval is large you run a larger
+ * risk for a not found error. This is because the index is cached and presetn, but if the client
+ * requests a non-cached updated file, this will not be present in the upstream.
+ * A smaller value will of course produce more server load.
+ */
 jobs {
 	deleteApkIndexFiles(delay: 0, interval: 60000) {
         alpineRepos = getAlpineRepos()
