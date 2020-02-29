5346cffadbbbaee0c8be6320d0a5c226fde24afb
Travis Foster <travisf@jfrog.com>
2020-02-28 16:45:01 -0800

    Fixed DBSyncNotification test for Artifactory 7

--- DB/DBSyncNotification/DBSyncNotificationTest.groovy
+++ DB/DBSyncNotification/DBSyncNotificationTest.groovy
@@ -5,7 +5,7 @@ class DBSyncNotificationTest extends Specification {
     def 'db sync notif test'() {
         when:
         def auth = "Basic ${'admin:password'.bytes.encodeBase64()}"
-        def logs = 'http://localhost:8088/artifactory/api/systemlogs/logData?id=artifactory.log'
+        def logs = 'http://localhost:8088/artifactory/api/systemlogs/downloadFile?id=console.log'
         def sync = 'http://localhost:8081/artifactory/api/plugins/execute/syncNotification'
         def lastline = null, conn = null, lastlinum = -1, startlinum = -1
         // the test won't pass unless we wait for the HA nodes to connect
@@ -13,12 +13,17 @@ class DBSyncNotificationTest extends Specification {
         while (true) {
             conn = new URL(logs).openConnection()
             conn.setRequestProperty('Authorization', auth)
+            if (conn.responseCode != 200) {
+                logs ='http://localhost:8088/artifactory/api/systemlogs/downloadFile?id=artifactory.log'
+                conn = new URL(logs).openConnection()
+                conn.setRequestProperty('Authorization', auth)
+            }
             assert conn.responseCode == 200
-            def oldlines = new JsonSlurper().parse(conn.inputStream).logContent.readLines()
+            def oldlines = new InputStreamReader(conn.inputStream).readLines()
             conn.disconnect()
             lastline = oldlines.findAll { it ==~ /.*\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.*/ }[-1]
             lastlinum = oldlines.findIndexOf { it == lastline }
-            startlinum = oldlines.findIndexOf { it.contains('Artifactory successfully started') }
+            startlinum = oldlines.findIndexOf { it.contains('All services started successfully') || it.contains('Artifactory successfully started') }
             if (lastlinum > startlinum) break
             if (System.currentTimeMillis() > starttime + 120000) {
                 if (startlinum > -1) {
@@ -39,7 +44,7 @@ class DBSyncNotificationTest extends Specification {
         conn = new URL(logs).openConnection()
         conn.setRequestProperty('Authorization', auth)
         assert conn.responseCode == 200
-        def lines = new JsonSlurper().parse(conn.inputStream).logContent.readLines()
+        def lines = new InputStreamReader(conn.inputStream).readLines()
         conn.disconnect()
         def idx = lines.findIndexOf { it == lastline }
         def newlines = lines[idx + 1 .. -1]
