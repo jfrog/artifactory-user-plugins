1537a2cc21501ea8c59cf6bc87fe77bb351575ad
Mohamamd Ali Toufighi <alitoumob@gmail.com>
2020-09-05 20:15:24 +0430

    improve logs

--- cleanup/cleanDockerImages/cleanDockerImages.groovy
+++ cleanup/cleanDockerImages/cleanDockerImages.groovy
@@ -79,7 +79,7 @@ def simpleTraverse(parentInfo, oldSet, maxUnusedSecondsAllowed) {
             && childItem.isFolder()
             && hasManifestJsonInChildren(currentPath)) {
             latestImageItemInfo = childItem
-            log.debug("Determined $latestImageItemInfo.repoPath.path as latest image for $parentInfo.name")
+            log.debug("Determined $latestImageItemInfo.repoPath.path as latest image for $parentInfo.repoPath.path")
             continue
         }
 
