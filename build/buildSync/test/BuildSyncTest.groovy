c3b00ce19e421eec75ec787298517b9e06dbf84b
Chris Bensen <>
2021-01-26 15:02:57 -0800

    Update buildSync to support Artifactory 7.

--- build/buildSync/test/BuildSyncTest.groovy
+++ build/buildSync/test/BuildSyncTest.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import spock.lang.Shared
 import spock.lang.Specification
 import groovy.json.JsonSlurper
@@ -14,7 +30,7 @@ class BuildSyncTest extends Specification {
     @Shared def artifactory2 = ArtifactoryClientBuilder.create().setUrl(baseurl2).setUsername('admin').setPassword('password').build()
 
     def setupSpec() {
-        // Crate replication user
+        // Create replication user
         def userb = artifactory2.security().builders().userBuilder()
         def builder = userb.name('sync').email('sync@foo.bar').admin(true)
         builder.password('password')
