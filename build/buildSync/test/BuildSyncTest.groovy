b991ccdecdbdc397c6859baea92110f9e96a0dbd
Chris Bensen <chrisb@jfrog.com>
2021-01-27 13:29:30 -0800

    Revert "Update buildSync to support Artifactory 7."
    This reverts commit 231c13ccc828aa41d08841493f3933410450f109.

--- build/buildSync/test/BuildSyncTest.groovy
+++ build/buildSync/test/BuildSyncTest.groovy
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2021 JFrog Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import spock.lang.Shared
 import spock.lang.Specification
 import groovy.json.JsonSlurper
@@ -30,7 +14,7 @@ class BuildSyncTest extends Specification {
     @Shared def artifactory2 = ArtifactoryClientBuilder.create().setUrl(baseurl2).setUsername('admin').setPassword('password').build()
 
     def setupSpec() {
-        // Create replication user
+        // Crate replication user
         def userb = artifactory2.security().builders().userBuilder()
         def builder = userb.name('sync').email('sync@foo.bar').admin(true)
         builder.password('password')
