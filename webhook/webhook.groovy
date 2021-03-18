99865fe15670c93d8da6f9d741d08405dac06195
Matthieu Serrepuy <lotooo@gmail.com>
2021-03-18 21:14:57 +0100

    webhooks: Create a formatter for keel.sh

--- webhook/webhook.groovy
+++ webhook/webhook.groovy
@@ -77,6 +77,9 @@ class Globals {
         default: [
             description: "The default formatter", formatter: new ResponseFormatter ( )
         ],
+        keel: [
+            description: "A POST formatted specifically for keel.sh", formatter: new KeelFormatter ( )
+        ],
         slack: [
             description: "A POST formatted specifically for Slack", formatter: new SlackFormatter ( )
         ],
@@ -301,6 +304,21 @@ class ResponseFormatter {
                     event: event,
                     data: data.content
             )
+        }
+        return builder
+    }
+}
+
+/**
+ * Keel formatter
+ */
+class KeelFormatter {
+    def format(String event, JsonBuilder data) {
+        def builder = new JsonBuilder()
+        def json = data.content
+        builder {
+          name "${WebHook.dockerRegistryUrl()}/${json.docker.image}"
+          tag json.docker.tag
         }
         return builder
     }
@@ -538,6 +556,7 @@ class WebHook {
     def debug = false
     def connectionTimeout = 15000
     def baseUrl
+    def dockerRegistryUrl
     def ctx = null
     def log = null
     // Used for async POSTS
@@ -580,6 +599,14 @@ class WebHook {
      */
     static String baseUrl() {
         return me.baseUrl
+    }
+
+    /**
+     * Get the dockerRegistryUrl value
+     * @return value if the dockerRegistryUrl value is set
+     */
+    static String dockerRegistryUrl() {
+        return me.dockerRegistryUrl
     }
 
     /**
@@ -794,6 +821,9 @@ class WebHook {
             // BaseUrl
             if (config.containsKey("baseurl"))
                 me.baseUrl = config.baseurl
+            // DockerRegistryUrl
+            if (config.containsKey("dockerRegistryUrl"))
+                me.dockerRegistryUrl = config.dockerRegistryUrl
         }
     }
 
