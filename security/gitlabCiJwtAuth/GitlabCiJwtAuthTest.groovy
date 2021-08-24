f68b5e1079104fc54ee9a2f8f0b523a7399661a1
Josh Perry <josh@6bit.com>
2021-08-24 12:25:30 -0600

    Fix test case and class name

--- security/gitlabCiJwtAuth/GitlabCiJwtAuthTest.groovy
+++ security/gitlabCiJwtAuth/GitlabCiJwtAuthTest.groovy
@@ -1,6 +1,6 @@
 import spock.lang.Specification
 
-class PluginTest extends Specification {
+class GitlabCiJwtAuthTest extends Specification {
     def 'not implemented plugin test'() {
         when:
         throw new Exception("Not implemented.")
