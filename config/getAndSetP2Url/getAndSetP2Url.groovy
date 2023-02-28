894d557a7c06a44cce00cf107e69fbd77778df95
Shivaram Radhakrishna <shivaramr@jfrog.com>
2023-02-27 18:00:51 -0600

    Adapt to changes in Artifactory internals

--- config/getAndSetP2Url/getAndSetP2Url.groovy
+++ config/getAndSetP2Url/getAndSetP2Url.groovy
@@ -17,13 +17,16 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import org.artifactory.addon.p2.P2Repo
-import org.artifactory.descriptor.config.CentralConfigDescriptor
+import org.artifactory.api.config.RepositoryConfigService
 import org.artifactory.descriptor.repo.RepoType
+import org.artifactory.model.VirtualRepoConfig
 import org.artifactory.repo.RepoPathFactory
+import org.artifactory.repo.packagetype.configuration.P2Configuration
 import org.artifactory.resource.ResourceStreamHandle
 import org.artifactory.ui.rest.model.admin.configuration.repository.virtual.VirtualRepositoryConfigModel
 import org.artifactory.ui.rest.service.admin.configuration.repositories.util.UpdateRepoConfigHelper
 import org.artifactory.ui.rest.service.admin.configuration.repositories.util.builder.RepoConfigModelBuilder
+import org.artifactory.util.P2PackageTypeConfigMapper
 
 /**
  * Test with
@@ -62,11 +65,16 @@ executions {
             message = msg
             return
         }
-        CentralConfigDescriptor config = ctx.centralConfig.descriptor
+        RepositoryConfigService repositoryConfigService = ctx.beanForType(RepositoryConfigService.class)
         status = 200
         def result = new P2UrlsConf()
         result.repo = repoKey
-        result.urls = config.getVirtualRepositoriesMap()?.get(repoKey)?.p2?.urls?.toArray(new String[0])
+        P2Configuration p2Configuration = null;
+        def repoConfig = repositoryConfigService.getRepoConfigByKey(repoKey)
+        if (repoConfig) {
+            p2Configuration = P2PackageTypeConfigMapper.INSTANCE.map(repoConfig.packageTypeConfig)
+        }
+        result.urls = p2Configuration?.urls?.toArray(new String[0])
         message = new JsonBuilder(result).toPrettyString()
     }
 
@@ -88,10 +96,10 @@ executions {
             return
         }
 
-        CentralConfigDescriptor config = ctx.centralConfig.descriptor
-        def repoDescriptor = config.getVirtualRepositoriesMap()?.get(input.repo)
-        // check that the repo descriptor is not NULL and that its a virtual repo. if not - return.
-        if (!repoDescriptor || repoDescriptor.type != RepoType.P2) {
+        RepositoryConfigService repositoryConfigService = ctx.beanForType(RepositoryConfigService.class)
+        VirtualRepoConfig virtualRepoConfig = repositoryConfigService.getVirtualRepoConfigByKey(input.repo)
+        // check that the repo config is not NULL and that its a virtual repo. if not - return.
+        if (!virtualRepoConfig || virtualRepoConfig.getPackageType() != RepoType.P2) {
             def msg = "The virtual repo ${input.repo} is not virtual or doesnt have a p2 configuration"
             log.warn(msg)
             status = 400
@@ -100,34 +108,37 @@ executions {
         }
 
         def model = new VirtualRepositoryConfigModel()
-        model = model.fromDescriptor(builder, repoDescriptor)
+        model = model.fromRepoConfig(builder, virtualRepoConfig)
 
         def p2Repos = Arrays.asList(input.urls).collect {
             def key = null
             if (it.startsWith('local://')) key = it - 'local://'
             else key = new URL(it).host
-            new P2Repo(null, key, it)
+            new P2Repo(null, null, key, it)
         }
 
         model.typeSpecific.p2Repos = p2Repos
         model.updateRepo(updater)
 
         // refetch and push the config, to get rid of any invalid repos
-        config = ctx.centralConfig.descriptor
-        repoDescriptor = config.getVirtualRepositoriesMap()?.get(input.repo)
+        virtualRepoConfig = repositoryConfigService.getVirtualRepoConfigByKey(input.repo)
         model = new VirtualRepositoryConfigModel()
-        model = model.fromDescriptor(builder, repoDescriptor)
+        model = model.fromRepoConfig(builder, virtualRepoConfig)
         model.updateRepo(updater)
 
-        config = ctx.centralConfig.descriptor
         status = 201
         def result = new P2UrlsConf()
         result.repo = input.repo
-        result.urls = config.virtualRepositoriesMap?.get(input.repo)?.p2?.urls?.toArray(new String[0])
+        P2Configuration p2Configuration = null;
+        def repoConfig = repositoryConfigService.getRepoConfigByKey(input.repo)
+        if (repoConfig) {
+            p2Configuration = P2PackageTypeConfigMapper.INSTANCE.map(repoConfig.packageTypeConfig)
+        }
+        result.urls = p2Configuration?.urls?.toArray(new String[0])
         message = new JsonBuilder(result).toPrettyString()
 
         // virtual repo needs to be cleaned
-        def rootPath = RepoPathFactory.create(repoDescriptor.getKey(), "")
+        def rootPath = RepoPathFactory.create(virtualRepoConfig.getKey(), "")
         repositories.deleteAtomic(rootPath)
     }
 }
